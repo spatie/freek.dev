4f45b279ca778aef5d73ac004d6cd813f06b3bae
freek <freek@spatie.be>
2017-11-17 09:44:15 +0100

    commit

--- database/seeds/TalkSeeder.php
+++ database/seeds/TalkSeeder.php
@@ -65,8 +65,58 @@ class TalkSeeder extends Seeder
                 'presented_at' => '2016-09-12',
                 'slides_link' => 'https://speakerdeck.com/freekmurze/backing-up-with-laravel-tokyo',
             ],
-
-
+            [
+                'title' => 'Taking care of backups with Laravel',
+                'location' => 'PHP Antwerp',
+                'presented_at' => '2016-06-29',
+                'slides_link' => 'https://speakerdeck.com/freekmurze/backing-up-with-laravel',
+                'joindin_link' => 'https://joind.in/event/php-antwerp---june-meetup/taking-care-of-backups-with-laravel',
+            ],
+            [
+                'title' => 'Creating a dashboard with Laravel and Vue',
+                'location' => 'PHP Antwerp',
+                'presented_at' => '2016-07-27',
+                'slides_link' => 'https://speakerdeck.com/freekmurze/creating-a-dashboard',
+            ],
+            [
+                'title' => 'Creating a dashboard with Laravel and Vue',
+                'location' => 'PHP Breda',
+                'presented_at' => '2016-07-27',
+            ],
+            [
+                'title' => 'Backing up with Laravel',
+                'location' => 'PHP Brussels',
+                'presented_at' => '2016-06-15',
+            ],
+            [
+                'title' => 'Backing up with Laravel',
+                'location' => 'PHP Gent',
+                'presented_at' => '2016-04-14',
+                'joindin_link' => 'https://joind.in/event/php-antwerp---june-meetup/taking-care-of-backups-with-laravel',
+            ],
+            [
+                'title' => 'Backing up with Laravel',
+                'location' => 'PHP Limburg',
+                'presented_at' => '2016-05-18',
+                'joindin_link' => 'https://joind.in/event/may-meetup---php-limburg-be/backing-up-with-laravel',
+            ],
+            [
+                'title' => 'Backing up with Laravel',
+                'location' => 'PHP West Flanders',
+                'presented_at' => '2016-06-28',
+            ],
+            [
+                'title' => 'Taking care of backups with Laravel',
+                'location' => 'php[world], Washington',
+                'presented_at' => '2016-11-16',
+                'https://speakerdeck.com/freekmurze/backing-up-with-laravel-phpworld',
+            ],
+            [
+                'title' => 'Creating a dashboard with Laravel and Vue',
+                'location' => 'php[world], Washington',
+                'presented_at' => '2016-11-17',
+                'https://speakerdeck.com/freekmurze/creating-a-dashboard-phpworld',
+            ],
         ]);
     }
 }
