622a6d3b32c4910fc0a6ae58d51bb2cc941a1efc
freek <freek@spatie.be>
2017-11-17 17:59:26 +0100

    commit

--- resources/views/front/newsletter/confirm.blade.php
+++ resources/views/front/newsletter/confirm.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Almost there…')
 
 @section('content')
-    <h1 class="pb-4 border-b">You’re almost there…<</h1>
+    <h1 class="pb-4 border-b">You’re almost there…</h1>
 
     <p class="pt-6">
         Thanks for subscribing to my newsletter. There’s an e-mail on it’s way to your address for verification. Just click
