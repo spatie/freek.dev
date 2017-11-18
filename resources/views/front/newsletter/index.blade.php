72fb6365ef23b98720c6e78d316f213da588242c
Jason Beggs <jasonlbeggs@gmail.com>
2017-11-17 21:28:39 -0500

    Improve styling of newsletter page subscribe form

--- resources/views/front/newsletter/index.blade.php
+++ resources/views/front/newsletter/index.blade.php
@@ -11,12 +11,12 @@
         day there is an emphasis on that framework.
     </p>
 
-    <form class="pt-4 pb-4" action="https://sendy.murze.be/subscribe" method="post" accept-charset="utf-8">
-        <div class="flex">
-            <input type="email" id="email" name="email" class="p-2 w-64 mr-2"
+    <form class="pt-4 pb-4 flex justify-center" action="https://sendy.murze.be/subscribe" method="post" accept-charset="utf-8">
+        <div class="flex flex-1 max-w-sm">
+            <input type="email" id="email" name="email" class="w-2/3 rounded xl:rounded-l 2xl:rounded border p-3 mr-2 lg:mr-0 lg:mb-1 xl:mb-0"
                    placeholder="Your email..." required="required">
             <input type="submit" name="submit" id="submit" value="Subscribe"
-                   class="bg-blue text-white py-2 px-4 text-xs font-bold clickable border-none rounded">
+                   class="w-1/3 rounded xl:rounded-r 2xl:rounded bg-blue text-white py-2 px-2 text-base font-bold clickable border-none">
             <input type="hidden" name="list" value="SGDpioFb8i8923zG5xWPFw5A">
         </div>
     </form>
