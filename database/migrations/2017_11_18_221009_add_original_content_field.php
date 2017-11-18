cbe8e25d42fa99b9a8d0352b8a5c01a9106848f9
freek <freek@spatie.be>
2017-11-18 23:29:48 +0100

    clean up migration

--- database/migrations/2017_11_18_221009_add_original_content_field.php
+++ database/migrations/2017_11_18_221009_add_original_content_field.php
@@ -6,25 +6,10 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddOriginalContentField extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
             $table->boolean('original_content')->default(false);
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
     }
 }
