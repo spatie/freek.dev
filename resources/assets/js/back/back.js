6b0704fb7903205f627c759e748d3ae88c3e9bc5
freek <freek@spatie.be>
2017-11-18 18:14:41 +0100

    commit

--- resources/assets/js/back/back.js
+++ resources/assets/js/back/back.js
@@ -5,7 +5,7 @@ let editors = document.getElementsByClassName('markdown-editor');
 if (editors.length) {
     new SimpleMDE({
         element: editors[0],
-        spellChecker: false,
+        spellChecker: true,
         autosave: {
             enabled: true,
             uniqueId: window.location,
