6e799e59213f5ada3c1f09984f442aad5ac1ed3e
freek <freek@spatie.be>
2017-11-20 20:49:45 +0100

    add time

--- resources/views/back/posts/_partials/form.blade.php
+++ resources/views/back/posts/_partials/form.blade.php
@@ -18,7 +18,7 @@
 
 <div class="form-line">
     <label class="form-label" for="publish_date">Publish date:</label>
-    <input class="form-text-input" name="publish_date" value="{{ old('publish_date', $post->publish_date->format('Y-m-d')) }}">
+    <input class="form-text-input" name="publish_date" value="{{ old('publish_date', $post->publish_date->format('Y-m-d H:i:s')) }}">
     @if($errors->has('publish_date'))
         <div class="validation-error">{{ $errors->first('publish_date') }}</div>
     @endif
