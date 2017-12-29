0493f63f7ab8da8d27e9f51354bab9a48da68f2b
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-12-30 01:57:26 +0200

    Fix rounded corners for newsletter widget

--- resources/views/front/layouts/_partials/newsletter.blade.php
+++ resources/views/front/layouts/_partials/newsletter.blade.php
@@ -3,10 +3,10 @@
 
     <form class="pt-4 pb-4" action="https://sendy.murze.be/subscribe" method="post" accept-charset="utf-8">
         <div class="flex flex-wrap">
-            <input type="email" id="email" name="email" class="rounded xl:rounded-l 2xl:rounded border p-2 mr-2 lg:mr-0 lg:mb-1 xl:mb-0 flex-1"
+            <input type="email" id="email" name="email" class="rounded xl:rounded-r-none 2xl:rounded border p-2 mr-2 lg:mr-0 lg:mb-1 xl:mb-0 flex-1"
                    placeholder="Your email…" required="required">
             <input type="submit" name="submit" id="submit" value="Subscribe"
-                   class="rounded xl:rounded-r 2xl:rounded bg-blue text-white py-2 px-2  text-xs font-bold clickable cursor-pointer border-none flex-1">
+                   class="rounded xl:rounded-l-none 2xl:rounded bg-blue text-white py-2 px-2  text-xs font-bold clickable cursor-pointer border-none flex-1">
             <input type="hidden" name="list" value="SGDpioFb8i8923zG5xWPFw5A">
         </div>
     </form>
