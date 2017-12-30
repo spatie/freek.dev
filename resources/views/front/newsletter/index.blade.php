0cfa03fc6533a64899d1b6945f0e7dcfbc28c792
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-12-30 16:59:11 +0200

    Fix rounded corners for newsletter form

--- resources/views/front/newsletter/index.blade.php
+++ resources/views/front/newsletter/index.blade.php
@@ -20,10 +20,10 @@
           accept-charset="utf-8">
         <div class="flex flex-1 max-w-sm">
             <input type="email" id="email" name="email"
-                   class="w-2/3 rounded xl:rounded-l 2xl:rounded border p-3 mr-2 lg:mr-0 lg:mb-1 xl:mb-0"
+                   class="w-2/3 rounded-l border p-3"
                    placeholder="Your email…" required="required">
             <input type="submit" name="submit" id="submit" value="Subscribe"
-                   class="w-1/3 rounded xl:rounded-r 2xl:rounded bg-blue text-white py-2 px-2 text-base font-bold clickable border-none">
+                   class="w-1/3 rounded-r bg-blue text-white py-2 px-2 text-base font-bold clickable border-none">
             <input type="hidden" name="list" value="SGDpioFb8i8923zG5xWPFw5A">
         </div>
     </form>
