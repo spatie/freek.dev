9daeb691961bfd5e1a408353b2d1f2a073f32bc5
freek <freek@spatie.be>
2018-01-02 18:57:56 +0100

    add tests

--- tests/Unit/Models/AdTest.php
+++ tests/Unit/Models/AdTest.php
@@ -19,6 +19,7 @@ class AdTest extends TestCase
         $this->assertNull(Ad::getForCurrentUrl());
 
         $this->setNow(2018, 1, 1);
+
         $this->assertEquals($januaryAd->id, Ad::getForCurrentUrl()->id);
 
         $this->setNow(2018, 1, 15);
@@ -40,9 +41,31 @@ class AdTest extends TestCase
         $this->assertNull(Ad::getForCurrentUrl());
     }
 
+    /** @test */
+    public function a_url_specific_ad_will_be_displayed_on_that_url()
+    {
+        $ad = $this->createAdForYearMonth(2018, 1, ['display_on_url' => 'test-url']);
+
+        $this->assertNull(Ad::getForCurrentUrl());
+
+        $this->assertEquals($ad->id, Ad::getForCurrentUrl('test-url')->id);
+    }
+
+    /** @test */
+    public function a_url_specific_ad_takes_precedence_over_the_general_one()
+    {
+        $urlSpecificAd = $this->createAdForYearMonth(2018, 1, ['display_on_url' => 'test-url']);
+
+        $generalAd = $this->createAdForYearMonth(2018, 1);
+
+        $this->assertEquals($urlSpecificAd->id, Ad::getForCurrentUrl('test-url')->id);
+
+        $this->assertEquals($generalAd->id, Ad::getForCurrentUrl('another-url')->id);
+    }
+
     protected function createAdForYearMonth(int $year, int $month, array $attributes = []): Ad
     {
-        $startsAt = Carbon::createFromDate($year, $month);
+        $startsAt = Carbon::createFromDate($year, $month, 1);
         $endsAt = $startsAt->copy()->endOfMonth();
 
         $defaultAttributes = [
