741caae12e5b1bbc5fd7ea5a652aba62e2fcf461
freek <freek@spatie.be>
2018-01-02 19:31:58 +0100

    fix formatting

--- resources/views/front/posts/detail.blade.php
+++ resources/views/front/posts/detail.blade.php
@@ -12,7 +12,7 @@
 
     @if($ad)
         <div class="bg-white border-2 rounded mb-4 py-4 px-6 mt-4 text-xs">
-            {{ $ad->text }} (sponsored link)
+            {!! $ad->text !!} (sponsored link)
         </div>
     @endif
 
