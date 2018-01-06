c207e0e7173ce4029740d1db050ade2499f7fb6f
freek <freek@spatie.be>
2018-01-06 10:16:00 +0100

    update ad page

--- resources/views/front/advertising/index.blade.php
+++ resources/views/front/advertising/index.blade.php
@@ -3,15 +3,35 @@
 @section('title', 'Advertising')
 
 @section('content')
+
     <h1 class="pb-4 border-b">Advertising</h1>
 
+    The murze.be blog and newsletter has adience of PHP developers with a heay interest in Laravel, JavaScript, devops and personal/professional growth. You can get your brand or product in front of that audience by running an ad on the blog and/or newsletter.
+
+    <p>
+        If you are interested in placing an advertisement on the blog or in the newsletter, or have any more questions, you can email
+        me at <a href="mailto:freek@spatie.be">freek@spatie.be</a>.
+    </p>
+
+    <h2>The blog</h2>
+    In 2017 the blog had 591 113 pageviews. If you want to view some more stastics and learn which we the most popular pages read <a href="https://murze.be/murzebe-turns-three">this blog post</a>.
+
+    <p>
+        An advertisement consists of a text with two or three
+        sentences that may contain links.. I’ll tag on ” (sponsored)” to the title so it’s clear to the readers that it’s an advertisement.
+        Because the blog should come over as very calm, images or logos are not allowed.
+    </p>
+
+    <p>An ad can be placed on all pages of the blog or one or more specific posts. It will be place in a white box with a smaller font right above the post tile. The ad will be displayed for at least a month.</p>
+
+    <h2>The newsletter</h2>
     <p class="pt-6">
         The murze.be newsletter is sent out every two weeks to an audience of PHP developers with a heavy interest in
         Laravel, JavaScript and devops.
     </p>
 
     <p>
-        You can get your brand or product in front of that audience by running an ad in the newsletter. An advertisement
+        An advertisement
         will run for an entire month (but keep in mind that the newsletter is sent out only once every two weeks).
     </p>
 
@@ -27,10 +47,5 @@
         I suspect that a large portion of the readers uses software that blocks trackers, so the actual open and click
         rates will probably be a bit higher. Rest assured that, because your ad will be text based, ad blockers will not
         detect or block it.
-    </p>
-
-    <p>
-        If you are interested in placing an advertisement in the newsletter, or have any more questions, you can email
-        me at <a href="mailto:freek@spatie.be">freek@spatie.be</a>.
     </p>
 @endsection
