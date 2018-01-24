89b52849d2173185f38a6c728430d6ce58fe362d
Freek Van der Herten <freek@spatie.be>
2018-01-24 11:05:05 +0100

    Use purgecss package (#27)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,8 @@
 const glob = require('glob-all');
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
-const purgeCss = require('purgecss-webpack-plugin');
+
+require('laravel-mix-purgecss');
 
 mix
     .js('resources/assets/js/front/front.js', 'public/js')
@@ -9,7 +10,7 @@ mix
     
     .sass('resources/assets/css/front/front.scss', 'public/css')
     .sass('resources/assets/css/back/back.scss', 'public/css')
-    
+
     .options({
         processCssUrls: false,
 
@@ -17,9 +18,9 @@ mix
             tailwindcss('tailwind.js'),
         ],
     })
-    
+
     .version()
-    
+
     .webpackConfig({
         output: {
             chunkFilename: 'js/[name].js',
@@ -30,28 +31,13 @@ mix
                 'vue$': 'vue',
             },
         },
+    })
 
-        plugins: [
-            new purgeCss({
-                paths: glob.sync([
-                    path.join(__dirname, 'app/**/*.php'),
-                    path.join(__dirname, 'resources/views/**/*.blade.php'),
-                    path.join(__dirname, 'resources/assets/js/**/*.vue'),
-                    path.join(__dirname, 'node_modules/simplemde/**/*.js'),
-                    path.join(__dirname, 'node_modules/turbolinks/**/*.js'),
-                    path.join(__dirname, 'vendor/spatie/menu/**/*.php'),
-                ]),
-                whitelistPatterns: [/carbon/, /language/, /hljs/, /cm-/, /alert-/],
-                extractors: [
-                    {
-                        extractor: class {
-                            static extract(content) {
-                                return content.match(/[A-z0-9-:\/]+/g) || []
-                            }
-                        },
-                        extensions: ['html', 'js', 'php', 'vue'],
-                    }
-                ]
-            })
+    .purgeCss({
+        globs: [
+            path.join(__dirname, 'node_modules/simplemde/**/*.js'),
+            path.join(__dirname, 'node_modules/turbolinks/**/*.js'),
+            path.join(__dirname, 'vendor/spatie/menu/**/*.php'),
         ],
+        whitelistPatterns: [/carbon/, /language/, /hljs/, /cm-/, /alert-/],
     });
