0ae33b5da570785df26d28e157424a4e65dd1804
freek <freek@spatie.be>
2018-02-18 12:27:01 +0100

    fix header

--- app/Http/Middleware/AddSecurityHeaders.php
+++ app/Http/Middleware/AddSecurityHeaders.php
@@ -18,7 +18,7 @@ class AddSecurityHeaders
         $response = $next($request);
 
         $response->headers->set('Referrer-Policy', 'strict-origin');
-        $response->headers->set('Strict-Transport-Security', 'strict-transport-security: max-age=31536000; includeSubDomains');
+        $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
 
         return $response;
     }
