69805e3937b0dedfa5ae403b691c42253f01b389
Lars Janssen <larsjanssen64@gmail.com>
2018-02-21 11:08:21 +0100

    Update content

--- resources/views/front/me/index.blade.php
+++ resources/views/front/me/index.blade.php
@@ -9,29 +9,29 @@
     @include('front.me._partials.freek')
 
     <p>
-        I live in Antwerp, Belgium and am passionate about all things PHP. I'm also a Laravel enthusiast and have used
-        the framework on many projects.  Follow me <a href="http://twitter.com/freekmurze">on twitter</a> to know what
+        I live in Antwerp, Belgium and am passionate about PHP. I'm a Laravel enthusiast and have used
+        the framework for many projects.  Follow me <a href="http://twitter.com/freekmurze">on twitter</a> to know what
         keeps me busy.
     </p>
 
     <p>
         Together with my buddies <a href="https://twitter.com/driesvints">Dries</a> and <a
                 href="https://twitter.com/maybefrederick">Frederick</a> I organise our local PHP user group <a
-                href="http://phpantwerp.be">PHP Antwerp</a>. If you are ever in the area of our beautiful city and what
+                href="http://phpantwerp.be">PHP Antwerp</a>. If you're ever in the area of our beautiful city and want
         to speak at our user group, let us know!
     </p>
 
     <p>
         At Spatie we use a lot of open source software: PHP, Ubuntu, Laravel, Composer, Beanstalkd,… are a few of the
-        things we use everyday. My company couldn't exists without open source software. That's why we try to give back
+        things we use everyday. My company couldn't exists without open source software. That's why we're trying to give back
         as much as possible.
     </p>
 
     <p>
-        Whenever we stumble upon a problem that we can solve in a clean way, we extract our solution for others to use.
+        Whenever we stumble upon a problem that we can solve in a clean way, we extract our solution so other developers can use it.
         Together with my colleagues I regularly release <a href="https://spatie.be/opensource">PHP, Laravel and
-            JavaScript packages</a>. Those packages have been downloaded a couple of millions times. Spatie is also
-        ranked as number 2 on <a href="http://git-awards.com/users?language=php">Git Awards' list of PHP developers
+            JavaScript packages</a>. Those packages have been downloaded a couple of million times. Spatie is 
+        ranked as number 4 on <a href="http://git-awards.com/users?language=php">Git Awards' list of PHP developers
             worldwide</a>.
     </p>
 
