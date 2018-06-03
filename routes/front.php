370be1f2ce615d1ad8ffa3d09bd82be148335d35
freek <freek@spatie.be>
2018-06-03 14:17:40 +0200

    commit

--- routes/front.php
+++ routes/front.php
@@ -1,19 +1,13 @@
 <?php
 
-Route::feeds();
-
-Route::get('/', 'HomeController@index');
-
-Route::get('/originals', 'OriginalsController@index');
-
-Route::get('tag/{tagSlug}', 'TaggedPostsController@index');
-
 Route::get('newsletter', 'NewsletterController@index');
 Route::get('confirm-your-email', 'NewsletterController@confirm');
 Route::get('subscribed', 'NewsletterController@subscribed');
 
+Route::feeds();
+Route::get('/', 'HomeController@index');
+Route::get('/originals', 'OriginalsController@index');
+Route::get('tag/{tagSlug}', 'TaggedPostsController@index');
 Route::get('me', 'MeController@index');
-
 Route::view('advertising', 'front.advertising.index');
-
 Route::get('{postSlug}', 'PostsController@detail');
