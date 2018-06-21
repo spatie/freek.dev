688d62936c705ad12f9bf1e1b55ccc1bf19367d1
freek <freek@spatie.be>
2018-06-21 21:37:47 +0200

    commit

--- resources/views/front/payments/_partials/chooseAmount.blade.php
+++ resources/views/front/payments/_partials/chooseAmount.blade.php
@@ -1,12 +1,12 @@
 <div>
-    Payments can be made with creditcards and are handled with securely via Stripe. Which amount would you
+    Payments can be made with creditcards and are handled with securely via Stripe. Which amount in EUR would you
     like to pay?
 </div>
 
 <form action="/payments/set-amount" method="POST" class="pt-2">
     @csrf
     <div class="flex flex-1">
-        <input class="rounded-l border p-3" name="amount in EUR" type="number" placeholder="Amount">
+        <input class="rounded-l border p-3" name="amount" type="number" placeholder="Amount in EUR">
         <input class="rounded-r bg-blue text-white py-2 px-2 text-base font-bold clickable border-none"
                value="Proceed to Stripe" type="submit">
     </div>
