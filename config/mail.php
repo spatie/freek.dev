af8184fe6e96fde2a2cb4cf81d04ecfe477249d8
freek <freek@spatie.be>
2018-06-21 21:33:33 +0200

    commit

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'freek@spatie.be'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
