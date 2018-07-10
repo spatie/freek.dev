c3d8b35d8327cebf74abcb5f7d95ebbbe654e323
Jason Beggs <jasonlbeggs@gmail.com>
2018-07-09 21:07:22 -0400

    Small UI improvements to forms and navigation

--- resources/views/front/payments/_partials/chooseAmount.blade.php
+++ resources/views/front/payments/_partials/chooseAmount.blade.php
@@ -1,13 +1,13 @@
-<div>
+<div class="lg:w-3/4 mb-6">
     Payments can be made with credit cards and are handled securely with Stripe. Which amount in EUR would you
     like to pay?
 </div>
 
-<form action="/payments/set-amount" method="POST" class="pt-2">
+<form action="/payments/set-amount" method="POST" class="pt-2 mb-8">
     @csrf
-    <div class="flex flex-1">
-        <input class="rounded-l border p-3" name="amount" type="number" placeholder="Amount in EUR">
-        <input class="rounded-r bg-blue text-white py-2 px-2 text-base font-bold clickable border-none"
+    <div class="flex flex-1 lg:w-3/4">
+        <input class="rounded-l border p-3 flex-1" name="amount" type="number" placeholder="Amount in EUR">
+        <input class="rounded-r bg-blue text-white py-2 px-6 text-base font-bold clickable border-none"
                value="Proceed to Stripe" type="submit">
     </div>
 
