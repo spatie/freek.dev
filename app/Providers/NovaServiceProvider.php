2348c97c1644d5eb2ab4df4aed41162fd319659b
freek <freek@spatie.be>
2018-08-24 23:48:05 +0200

    add analytics

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -56,7 +56,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function cards()
     {
         return [
-            //new Help,
+            new \Tightenco\NovaGoogleAnalytics\PageViewsMetric(),
         ];
     }
 
