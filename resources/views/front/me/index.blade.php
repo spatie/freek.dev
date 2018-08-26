35473da00c106d88d17a16ff3428511f4191ab3d
freek <freek@spatie.be>
2018-08-26 11:37:46 +0200

    update bio

--- resources/views/front/me/index.blade.php
+++ resources/views/front/me/index.blade.php
@@ -15,14 +15,7 @@
     </p>
 
     <p>
-        Together with my buddies <a href="https://twitter.com/driesvints">Dries</a> and <a
-                href="https://twitter.com/maybefrederick">Frederick</a> I organise our local PHP user group <a
-                href="http://phpantwerp.be">PHP Antwerp</a>. If you're ever in the area of our beautiful city and want
-        to speak at our user group, let us know!
-    </p>
-
-    <p>
-        At Spatie we use a lot of open source software: PHP, Ubuntu, Laravel, Composer, Beanstalkd,… are a few of the
+        I'm a developer at <a href="https://spatie.be">Spatie</a> of which I'm the co-owner. At my company we use a lot of open source software: PHP, Ubuntu, Laravel, Composer, Beanstalkd,… are a few of the
         things we use everyday. My company couldn't exists without open source software. That's why we're trying to give back
         as much as possible.
     </p>
@@ -30,15 +23,26 @@
     <p>
         Whenever we stumble upon a problem that we can solve in a clean way, we extract our solution so other developers can use it.
         Together with my colleagues I regularly release <a href="https://spatie.be/opensource">PHP, Laravel and
-            JavaScript packages</a>. Those packages have been downloaded a couple of million times. Spatie is 
+            JavaScript packages</a>. Those packages have been downloaded a couple of million times. Spatie is
         ranked as number 4 on <a href="http://git-awards.com/users?language=php">Git Awards' list of PHP developers
             worldwide</a>.
     </p>
 
     <p>
-        I love public speaking and try to do it a lot. Here's a list of past user group meetups and conferences where I had the pleasure of speaking.
+        Out side of Spatie, my fried <a href="https://twitter.com/mattiasgeniar">Mattias</a> and I are running a Saas named <a href="https://ohdear.app">Oh Dear!</a> Our service can notify you via Mail, Slack,... when your site is down, when it contains broken links, when it finds mixed content, ... It aims to be very easy to use, we have great developer docs and APIs. <a href="https://ohdear.app/register">Try it out!</a>
+    </p>
+
+    <p>
+        Together with my buddies <a href="https://twitter.com/driesvints">Dries</a> and <a
+                href="https://twitter.com/maybefrederick">riasvdv</a> I organise the <a
+                href="http://fullstackantwerpt.be">Full Stack Antwerp</a> usergroup. If you're ever in the area of our beautiful city and want to speak at our user group, let us know!
+    </p>
+
+    <p>Together with the aforementioned Dries, I'm also organising the Full Stack Europe conference in the city of Antwerp. Full Stack Europe will be a conference for every kind of developer. We will offer talks for developers who want to learn across a wide variety of skills. We plan on holding it somewhere in 2019. For more info read <a href="https://blog.usejournal.com/announcing-full-stack-europe-f49422f14308">Dries' announcment blogpost</a> All you need to do to stay in the loop is to subscribe yourself <a href="https://fullstackeurope.com/">on our mailinglist</a></p>
+
+    <p>
+        I also love public speaking and try to do it a lot. Here's a list of past user group meetups and conferences where I had the pleasure of speaking.
     </p>
 
     @include('front.me._partials.talks')
 @endsection
-
