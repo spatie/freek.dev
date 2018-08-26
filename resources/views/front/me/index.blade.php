f123e557db53cc3fd0a8154123592ae18ab4017d
freek <freek@spatie.be>
2018-08-26 11:43:29 +0200

    fix typo

--- resources/views/front/me/index.blade.php
+++ resources/views/front/me/index.blade.php
@@ -38,7 +38,7 @@
     <p>
         Together with my buddies <a href="https://twitter.com/driesvints">Dries</a> and <a
                 href="https://twitter.com/maybefrederick">riasvdv</a> I organise the <a
-                href="http://fullstackantwerpt.be">Full Stack Antwerp</a> usergroup. If you're ever in the area of our beautiful city and want to speak at our user group, let us know!
+                href="http://fullstackantwerp.be">Full Stack Antwerp</a> usergroup. If you're ever in the area of our beautiful city and want to speak at our user group, let us know!
     </p>
 
     <p>Together with the aforementioned Dries, I'm also organising the Full Stack Europe conference in the city of Antwerp. Full Stack Europe will be a conference for every kind of developer. We will offer talks for developers who want to learn across a wide variety of skills. We plan on holding it somewhere in 2019. For more info read <a href="https://blog.usejournal.com/announcing-full-stack-europe-f49422f14308">Dries' announcment blogpost</a> All you need to do to stay in the loop is to subscribe yourself <a href="https://fullstackeurope.com/">on our mailinglist</a></p>
