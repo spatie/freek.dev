ff230350925d835dfee7392eadd60c454b671198
freek <freek@spatie.be>
2018-08-27 21:54:57 +0200

    fix typo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $posts = Post::quer()
+        $posts = Post::query()
             ->published()
             ->orderBy('publish_date', 'desc')
             ->orderBy('id', 'desc')
