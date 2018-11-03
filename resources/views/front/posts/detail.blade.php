c5729ac1abd7e9c4ea4be70777b118469c53e1f5
freek <freek@spatie.be>
2018-11-03 19:26:23 +0100

    commit

--- resources/views/front/posts/detail.blade.php
+++ resources/views/front/posts/detail.blade.php
@@ -30,17 +30,9 @@
         @include('front.posts._partials.tags')
     </div>
 
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- Detail pages -->
-    <ins class="adsbygoogle"
-         style="display:block"
-         data-ad-client="ca-pub-2689560793157981"
-         data-ad-slot="2810918527"
-         data-ad-format="auto"
-         data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+    <div class="pt-4">
+        @include('front.posts._partials.adsense')
+    </div>
 
     <div class="pt-4">
         @include('front.posts._partials.disqus')
