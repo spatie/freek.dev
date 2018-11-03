e62159181c15d991322c64e6d52909c23e8044c2
freek <freek@spatie.be>
2018-11-03 19:39:47 +0100

    commit

--- resources/views/front/layouts/_partials/adsense.blade.php
+++ resources/views/front/layouts/_partials/adsense.blade.php
@@ -1,11 +1,9 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Detail pages -->
+<!-- Ad -->
 <ins class="adsbygoogle"
-     style="display:block"
+     style="display:inline-block;width:261px;height:219px"
      data-ad-client="ca-pub-2689560793157981"
-     data-ad-slot="2810918527"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
+     data-ad-slot="5078947073"></ins>
 <script>
     (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
