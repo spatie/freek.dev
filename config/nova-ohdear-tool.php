dfa06eac24241f272e321caad24daacee15094f4
freek <freek@spatie.be>
2018-11-10 00:09:16 +0100

    commit

---
+++ config/nova-ohdear-tool.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+     * A valid API token for your Oh Dear! account. Instructions on how to get a
+     * token can be found on this page: https://ohdear.app/docs/api/authentication
+     */
+    'api_token' => env('OH_DEAR_API_TOKEN'),
+
+    /*
+     * The id of the site you want to display in the tool.
+     */
+    'site_id' => env('OH_DEAR_SITE_ID'),
+];
