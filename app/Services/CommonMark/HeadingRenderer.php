b21c5e7149366795ffb80e186ce630ad2460216e
freek <freek@spatie.be>
2018-11-29 19:14:46 +0100

    wip

--- app/Services/CommonMark/HeadingRenderer.php
+++ app/Services/CommonMark/HeadingRenderer.php
@@ -11,7 +11,6 @@ class HeadingRenderer extends BaseHeadingRenderer
 {
     public function render(AbstractBlock $block, ElementRendererInterface $htmlRenderer, $inTightList = false)
     {
-
         $element = parent::render($block, $htmlRenderer, $inTightList);
 
         $id = str_slug($element->getContents());
@@ -19,9 +18,22 @@ class HeadingRenderer extends BaseHeadingRenderer
         $element->setAttribute('id', $id);
         $element->setContents(
             $element->getContents() .
-            new HtmlElement('a', ['href' => "#{$id}", 'class' => 'anchor-link'], ' #')
+            new HtmlElement('a', ['href' => "#{$id}", 'class' => $this->getFragmentLinkClass($element->getTagName())], ' #')
         );
 
         return $element;
+    }
+
+    protected function getFragmentLinkClass($elementName)
+    {
+        if ($elementName === 'h1') {
+            return 'text-grey';
+        }
+
+        if ($elementName === 'h2') {
+            return 'text-grey';
+        }
+
+        return 'text-grey-light';
     }
 }
