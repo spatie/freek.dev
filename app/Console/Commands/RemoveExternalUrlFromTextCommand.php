f3705d8e364f703b33acdc1da5dc64e36c250159
freek <freek@spatie.be>
2019-04-23 01:24:04 +0200

    wip

---
+++ app/Console/Commands/RemoveExternalUrlFromTextCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Post;
+use Illuminate\Console\Command;
+
+class RemoveExternalUrlFromTextCommand extends Command
+{
+    protected $signature = 'blog:remove-external-url-from-text';
+
+    protected $description = 'Remove the external url from the text field';
+
+    public function handle()
+    {
+        Post::query()->whereNotNull('external_url')->each(function (Post $post) {
+            $post->text = $this->getSanitizedText($post);
+
+            $post->save();
+        });
+    }
+
+    private function getSanitizedText(Post $post): string
+    {
+        $text = $post->text;
+
+        $text = str_replace("[$post->external_url]($post->external_url)", '', $text);
+
+        $text = str_replace($post->external_url, '', $text);
+
+        return trim($text);
+    }
+}
