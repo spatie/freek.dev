81aa9a4d1b6bec11e5043c8bdf4ed504c980ff51
freek <freek@spatie.be>
2019-05-18 14:43:04 +0200

    wip

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,9 +29,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             CacheControl::class,
-            //\App\Http\Middleware\Turbolinks::class,
-            //AddCspHeaders::class,
-            //AddSecurityHeaders::class,
+            \Spatie\ResponseCache\Middlewares\CacheResponse::class
         ],
 
         'api' => [
