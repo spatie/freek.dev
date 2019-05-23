843c24d9e7fd4f06ddcdee25134103c32216f89e
freek <freek@spatie.be>
2019-05-24 01:20:19 +0200

    wip

--- resources/views/front/about/index.blade.php
+++ resources/views/front/about/index.blade.php
@@ -22,10 +22,7 @@
             Outside of Spatie, my friend <a href="https://twitter.com/mattiasgeniar">Mattias</a> and I are running a Saas named <a href="https://ohdear.app">Oh Dear!</a> Our service can notify you via Mail, Slack,... when your site is down, when it contains broken links, when it finds mixed content, ... It aims to be very easy to use, we have great developer docs and APIs. <a href="https://ohdear.app/register">Try it out!</a>
         </p>
         <p>
-            Together with my buddies <a href="https://twitter.com/driesvints">Dries</a> and <a href="https://twitter.com/riasvdv">Rias</a> I organise the <a href="http://fullstackantwerp.be">Full Stack Antwerp</a> usergroup. If you're ever in the area of our beautiful city and want to speak at our user group, let us know!
-        </p>
-        <p>
-            Together with the aforementioned Dries, I'm also organising the Full Stack Europe conference in the city of Antwerp. Full Stack Europe will be a conference for every kind of developer. We will offer talks for developers who want to learn across a wide variety of skills. We plan on holding it somewhere in 2019. For more info read <a href="https://blog.usejournal.com/announcing-full-stack-europe-f49422f14308">Dries' announcement blogpost</a> All you need to do to stay in the loop is to subscribe yourself <a href="https://fullstackeurope.com/">on our mailinglist</a>.
+            Together with my buddy <a href="https://twitter.com/driesvints">Dries</a>  I organise the <a href="http://fullstackeurope.com">Full Stack Europe</a> conference. It will be a conference for every kind of developer. We will offer talks for developers who want to learn across a wide variety of skills. We plan on holding it somewhere in 2019. For more info read <a href="https://blog.usejournal.com/announcing-full-stack-europe-f49422f14308">Dries' announcement blogpost</a> All you need to do to stay in the loop is to subscribe yourself <a href="https://fullstackeurope.com/">on our mailinglist</a>.
         </p>
         <p>
             I also love public speaking and try to do it a lot. Here's a list of past user group meetups and conferences where I had the pleasure of speaking.
