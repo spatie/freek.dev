408cb3b6b0e6c2f9850cb57db472772183a7bf46
freek <freek@spatie.be>
2019-05-24 01:15:01 +0200

    wip

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,7 +2,9 @@
 
 use App\Models\Post;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
+/* @var Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(Post::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence,
--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -4,7 +4,6 @@ use Faker\Generator as Faker;
 use Spatie\Tags\Tag;
 
 /* @var Illuminate\Database\Eloquent\Factory $factory */
-
 $factory->define(Tag::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
