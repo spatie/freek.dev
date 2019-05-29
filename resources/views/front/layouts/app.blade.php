089e08cedb9d9b32ea502910b300ffc9e9104240
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2019-05-29 19:43:13 +0200

    wip

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -1,23 +1,4 @@
-<<<<<<< HEAD:resources/views/front/layouts/master.blade.php
-<!DOCTYPE html>
-
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="mobile-web-app-capable" content="yes">
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-
-<title>@yield('title') | Freek Van der Herten's blog on PHP and Laravel</title>
-
-@include('feed::links')
-@include('front.layouts.partials.seo')
-
-<link rel="stylesheet" href="https://cloud.typography.com/6194432/6581412/css/fonts.css"/>
-<link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-<script src="{{ mix('js/app.js') }}" defer></script>
-=======
 @include('front.layouts.partials.head')
->>>>>>> Error pages:resources/views/front/layouts/app.blade.php
 
 <div class="font-sans text-black">
     @include('front.layouts.partials.analytics')
