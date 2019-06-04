14563644427c7db14b2a13ea1660fbe658461a0d
freek <freek@spatie.be>
2019-06-04 14:15:37 +0200

    wip

--- resources/views/front/about/index.blade.php
+++ resources/views/front/about/index.blade.php
@@ -16,8 +16,7 @@
             I live in Ghent, Belgium and am passionate about PHP. I'm a Laravel enthusiast and have used the framework for many projects.  Follow me <a href="http://twitter.com/freekmurze">on Twitter</a> to know what keeps me busy. Want to know which IDE, apps and hardware I use? <a href="/my-current-setup-2018-edition">Here you go</a>!
         </p>
         <p>
-            I'm a developer at <a href="https://spatie.be">Spatie</a> of which I'm the co-owner. At my company we use a lot of open source software: PHP, Ubuntu, Laravel, Composer, Beanstalkd,… are a few of the things we use everyday. My company couldn't exist without open source software. That's why we're trying to give back
-            as much as possible.
+            I'm a developer at <a href="https://spatie.be">Spatie</a> of which I'm the co-owner. At my company we use a lot of open source software: PHP, Ubuntu, Laravel, Composer, Yarn,… are a few of the things we use everyday. My company couldn't exist without open source software. That's why we're trying to give back as much as possible.
         </p>
         <p>
             Whenever we stumble upon a problem that we can solve in a clean way, we extract our solution so other developers can use it. Together with my colleagues I regularly release <a href="https://spatie.be/opensource">PHP, Laravel and JavaScript packages</a>. Those packages have been downloaded more than 35 million times.
@@ -29,7 +28,7 @@
             Outside of Spatie, my friend <a href="https://twitter.com/mattiasgeniar">Mattias</a> and I are running a Saas named <a href="https://ohdear.app">Oh Dear!</a> Our service can notify you via Mail, Slack,... when your site is down, when it contains broken links, when it finds mixed content, ... It aims to be very easy to use, we have great developer docs and APIs. <a href="https://ohdear.app/register">Try it out!</a>
         </p>
         <p>
-            Together with my buddy <a href="https://twitter.com/driesvints">Dries</a>  I organise the <a href="http://fullstackeurope.com">Full Stack Europe</a> conference. It will be a conference for every kind of developer. We will offer talks for developers who want to learn across a wide variety of skills. We plan on holding it somewhere in 2019. For more info read <a href="https://blog.usejournal.com/announcing-full-stack-europe-f49422f14308">Dries' announcement blogpost</a> All you need to do to stay in the loop is to subscribe yourself <a href="https://fullstackeurope.com/">on our mailinglist</a>.
+            Together with my buddy <a href="https://twitter.com/driesvints">Dries</a>  I organise the <a href="https://fullstackeurope.com">Full Stack Europe</a> conference. We'll offer talks for developers who want to learn across a wide variety of skills. For more info head over to <a href="https://fullstackeurope.com">our website</a>.
         </p>
     </div>
 @endsection
