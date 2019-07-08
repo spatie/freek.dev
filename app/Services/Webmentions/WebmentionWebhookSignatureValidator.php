af556da6852bb7ebded1d95a548ea810e12b0886
freek <freek@spatie.be>
2019-07-08 21:34:21 +0200

    wip

--- app/Services/Webmentions/WebmentionWebhookSignatureValidator.php
+++ app/Services/Webmentions/WebmentionWebhookSignatureValidator.php
@@ -11,13 +11,10 @@ class WebmentionWebhookSignatureValidator implements SignatureValidator
 {
     public function isValid(Request $request, WebhookConfig $config): bool
     {
-        \Log::info(print_r($request->all, true));
-
-        return true;
         if (! $request->has('secret')) {
             return false;
         }
 
-        return $request->secret !== config('services.webmentions.webhook_secret');
+        return $request->secret === config('services.webmentions.webhook_secret');
     }
 }
