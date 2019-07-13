d9d09749b53742d2be5953033f713f680d984cda
freek <freek@spatie.be>
2019-07-14 00:36:29 +0200

    wip

--- resources/views/front/posts/partials/comments.blade.php
+++ resources/views/front/posts/partials/comments.blade.php
@@ -1,7 +1,9 @@
 @if (! $post->tweet_url)
-    @component('front.components.lazy')
-        @include('front.posts.partials.disqus')
-    @endcomponent
+    <div class="mb-8">
+        @component('front.components.lazy')
+            @include('front.posts.partials.disqus')
+        @endcomponent
+    </div>
 @endif
 
 @include('front.posts.partials.webmentions')
