3d893d97eecb459a28380bb37862c23d4b96655b
freek <freek@spatie.be>
2019-09-09 11:46:11 +0200

    wip

--- config/cache.php
+++ config/cache.php
@@ -72,7 +72,7 @@ return [
         ],
 
         'redis' => [
-            'driver' => 'predis',
+            'driver' => 'redis',
             'connection' => 'cache',
         ],
 
--- config/database.php
+++ config/database.php
@@ -119,10 +119,10 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'cluster' => env('REDIS_CLUSTER', 'predis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
