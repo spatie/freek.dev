495e25c7a7789e79b96829564bd3e3523519b0fc
freek <freek@spatie.be>
2019-09-14 20:27:46 +0200

    add honeypot field

--- resources/views/front/newsletter/partials/form.blade.php
+++ resources/views/front/newsletter/partials/form.blade.php
@@ -6,6 +6,6 @@
 >
     <input type="email" id="email" name="email" placeholder="Your e-mail address" class="flex-1 px-3 py-2 bg-white focus:outline-none focus:border-gray-400 border-y-3 border-t-transparent mb-2 md:mb-0" aria-label="E-mail" required>
     <input type="hidden" name="list" value="SGDpioFb8i8923zG5xWPFw5A">
-    <input type="hidden" name="hp" />
+    <input class="hidden" type="text" name="hp" />
     <input type="submit" name="submit" id="submit" value="Subscribe" class="px-3 py-2 text-sm text-white bg-orange-500 font-semibold border-y-3 border-orange-700 border-t-transparent">
 </form>
