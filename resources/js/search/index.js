ce0bcc138c65ec4ea419bb4ffc1dc194f137a3d6
freek <freek@spatie.be>
2019-09-18 15:13:58 +0200

    wip

--- resources/js/search/index.js
+++
@@ -1,8 +0,0 @@
-import React, { render } from 'preact-compat';
-import Search from './Search';
-
-export function mount(container) {
-    const inputClassName = container.querySelector('input').className;
-
-    render(<Search inputClassName={inputClassName} {...container.dataset} />, container);
-}
---
+++ resources/views/livewire/search.blade.php
@@ -0,0 +1,24 @@
+<input wire:model="query"
+       type="text"
+       class="bg-gray-100 px-3 pb-2 pt-3 w-full focus:outline-none border-gray-200 focus:border-gray-300 border-y-4 border-t-transparent"
+>
+
+Dit is de query {{ $query ?? 'no query' }}
+
+@if (count($results))
+    <ul>
+        @foreach($results as $post)
+            <li>
+                <strong className="text-lg">
+                    <a href={{ $post->url }}>{{ $post->title }}</a>
+                </strong>
+                <br/>
+                <a href="{{ $post->url }}" className="text-sm text-gray-700">
+                    {{ $post->formatted_type }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+@else
+    <p class="mt-2 text-gray-700">Nothing here…</p>
+@endif
