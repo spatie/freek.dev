ae801de534c71cef45c8c7c5a278bed97598ca35
freek <freek@spatie.be>
2019-09-18 15:31:48 +0200

    wip

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -3,8 +3,6 @@
        class="bg-gray-100 px-3 pb-2 pt-3 w-full focus:outline-none border-gray-200 focus:border-gray-300 border-y-4 border-t-transparent"
 >
 
-Dit is de query {{ $query ?? 'no query' }}
-
 @if (count($results))
     <ul>
         @foreach($results as $post)
