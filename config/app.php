ea4dc702952d4d22dac50117d5e7bce3b86d63e4
freek <freek@spatie.be>
2019-10-11 10:44:39 +0200

    wip

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        \App\Providers\FlashServiceProvider::class,
 
         /*
          * Package Service Providers...
