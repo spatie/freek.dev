cc29ddc4efd8ac58bbc5fe2b79a29aca54c42a7e
freek <freek@spatie.be>
2019-11-12 21:05:55 +0100

    wip

--- database/seeds/EmailListSeeder.php
+++ database/seeds/EmailListSeeder.php
@@ -2,15 +2,24 @@
 
 
 use Illuminate\Database\Seeder;
-use Spatie\MailCoach\Models\EmailList;
+use Spatie\Mailcoach\Models\EmailList;
+use Spatie\Mailcoach\Models\Template;
 
 class EmailListSeeder extends Seeder
 {
     public function run()
     {
-        EmailList::create([
+        /** @var \Spatie\Mailcoach\Models\EmailList $emailList */
+        $emailList = EmailList::create([
             'name' => 'freek.dev newsletter',
             'requires_double_opt_in' => true,
+        ]);
+
+        $emailList->subscribeNow('freek@spatie.be');
+
+        Template::create([
+            'name' => 'test',
+            'html' => '<html><body><a href="https://spatie.be">Spatie</a></body></html>'
         ]);
     }
 }
