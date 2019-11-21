3cba71eab8b1c1e91bca5067b4bd6a7bc1ed0f93
freek <freek@spatie.be>
2019-11-21 14:37:13 +0100

    wip

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -5,23 +5,27 @@
            class="bg-gray-100 px-3 pb-2 pt-3 w-full focus:outline-none border-gray-200 focus:border-gray-300 border-y-4 border-t-transparent mb-4"
     >
 
-    @if ($query !== '')
-        @if (count($results))
-            <ul>
-                @foreach($results as $post)
-                    <li class="mb-6">
-                        <strong class="text-lg">
-                            <a href={{ $post->url }}>{{ $post->title }}</a>
-                        </strong>
-                        <br/>
-                        <a href="{{ $post->url }}" class="text-sm text-gray-700">
-                            {{ $post->formatted_type }} - {{ $post->publish_date->format('M jS Y') }}
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-        @else
-            <p class="mt-2 text-gray-700">Nothing here…</p>
+    @if ($query === 'greece woods')
+        🌳 We love you Greece! 🇬🇷
+    @else
+        @if ($query !== '')
+            @if (count($results))
+                <ul>
+                    @foreach($results as $post)
+                        <li class="mb-6">
+                            <strong class="text-lg">
+                                <a href={{ $post->url }}>{{ $post->title }}</a>
+                            </strong>
+                            <br/>
+                            <a href="{{ $post->url }}" class="text-sm text-gray-700">
+                                {{ $post->formatted_type }} - {{ $post->publish_date->format('M jS Y') }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            @else
+                <p class="mt-2 text-gray-700">Nothing here…</p>
+            @endif
         @endif
     @endif
 </div>
