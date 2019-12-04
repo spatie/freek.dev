767598b658de949db73702ef8630112606697f5a
freek <freek@spatie.be>
2019-12-04 21:04:52 +0100

    wip

--- resources/views/mail/welcome.blade.php
+++ resources/views/mail/welcome.blade.php
@@ -1,14 +1,13 @@
-@component('mail::message')
 Hi,
 
 welcome to the freek.dev newsletter.  Every two weeks you can expect some nice links to cool stuff on Laravel, PHP and JavaScript in your mailbox.
 
-If you already want to read some stuff here are links to the latest newsletters:
+If you already want to read some stuff, here are links to the latest newsletters:
 @foreach($newsletters as $newsletter)
 - [{{ $newsletter->title }}]({{ $newsletter->url }}) - sent on {{ $newsletter->sent_at->format('jS F Y') }}
 @endforeach
 
-Here are some blogposts I recently published on [freek.dev](https://freek.dev):
+Here are some blog posts I recently published on [freek.dev](https://freek.dev):
 @foreach($posts as $post)
 - [{{ $post->title }}]({{ $post->url }})
 @endforeach
@@ -17,4 +16,3 @@ Thanks for subscribing!
 
 
 Freek
-@endcomponent
