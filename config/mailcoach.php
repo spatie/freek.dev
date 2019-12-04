165f94a9ab2dc4b2bf582c3e88b3ff43e24c4a05
freek <freek@spatie.be>
2019-12-04 16:37:09 +0100

    wip

--- config/mailcoach.php
+++ config/mailcoach.php
@@ -42,6 +42,7 @@ return [
         'create_subscriber_action' => \Spatie\Mailcoach\Actions\CreateSubscriberAction::class,
         'confirm_subscription_action' => \Spatie\Mailcoach\Actions\ConfirmSubscriptionAction::class,
         'convert_html_to_text' => \Spatie\Mailcoach\Actions\ConvertHtmlToTextAction::class,
+        'send_welcome_mail_action' => \Spatie\Mailcoach\Actions\SendWelcomeMailAction::class,
     ],
 
     'mailgun_feedback' => [
