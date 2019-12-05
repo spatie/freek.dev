2738662b0513f2b8113199bb1b276942d13791f4
freek <freek@spatie.be>
2019-12-05 03:25:11 +0100

    wip

--- app/Mail/WelcomeMail.php
+++ app/Mail/WelcomeMail.php
@@ -2,23 +2,23 @@
 
 namespace App\Mail;
 
-use App\Models\Newsletter;
 use App\Models\Post;
 use Illuminate\Support\Collection;
 use Spatie\Mailcoach\Mails\WelcomeMail as MailcoachWelcomeMail;
+use Spatie\Mailcoach\Models\Campaign;
 use Spatie\Mailcoach\Models\Subscriber;
 
 class WelcomeMail extends MailcoachWelcomeMail
 {
     public Collection $posts;
 
-    public Collection $newsletters;
+    public Collection $campaigns;
 
     public function __construct(Subscriber $subscriber)
     {
         parent::__construct($subscriber);
 
-        $this->newsletters = Newsletter::orderByDesc('sent_at')->take(3)->get();
+        $this->campaigns = Campaign::sent()->orderByDesc('sent_at')->limit(3)->get();
 
         $this->posts = Post::published()->originalContent()->orderByDesc('publish_date')->limit(10)->get();
     }
--- resources/views/mail/welcome.blade.php
+++ resources/views/mail/welcome.blade.php
@@ -4,8 +4,8 @@ Hi,
 welcome to the freek.dev newsletter.  Every two weeks you can expect some nice links to cool stuff on Laravel, PHP and JavaScript in your mailbox.
 
 If you already want to read some stuff, here are links to the latest newsletters:
-@foreach($newsletters as $newsletter)
-- [{{ $newsletter->title }}]({{ $newsletter->url }}) - sent on {{ $newsletter->sent_at->format('jS F Y') }}
+@foreach($campaigns as $campaign)
+- [{{ $campaign->title }}]({{ $campaign->webviewUrl() }}) - sent on {{ $campaign->sent_at->format('jS F Y') }}
 @endforeach
 
 Here are some blog posts I recently published on [freek.dev](https://freek.dev):
