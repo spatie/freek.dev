d8d85a19cbecfd306d6403a007ac8b45c72a621e
freek <freek@spatie.be>
2019-12-08 10:18:30 +0100

    wip

--- resources/views/front/newsletter/confirmed.blade.php
+++ resources/views/front/newsletter/confirmed.blade.php
@@ -9,7 +9,7 @@
             Awesome! You are now subscribed to my newsletter. Every two weeks you can expect some nice links to cool stuff on Laravel, PHP and JavaScript in your mailbox.
         </p>
         <p>
-            By clicking [this link](https://twitter.com/intent/tweet?url=https%3A%2F%2Ffreek.dev%2Fnewsletter&text=I%27ve%20just%20signed%20up%20for%20the%20newsletter%20on%20Laravel%2C%20PHP%20and%20JavaScript%20by%20@freekmurze.%20You%20can%20sign%20up%20via%20this%20link%3A&hashtags=laravel%2C%20php%2C%20javascript) you can share your subscription with your followers on Twitter.
+            By clicking <a href="https://twitter.com/intent/tweet?url=https%3A%2F%2Ffreek.dev%2Fnewsletter&text=I%27ve%20just%20signed%20up%20for%20the%20newsletter%20on%20Laravel%2C%20PHP%20and%20JavaScript%20by%20@freekmurze.%20You%20can%20sign%20up%20via%20this%20link%3A&hashtags=laravel%2C%20php%2C%20javascript">this link</a> you can share your subscription with your followers on Twitter.
         </p>
     </div>
 @endsection
