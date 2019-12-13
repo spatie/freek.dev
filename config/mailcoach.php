01d84f6fade60cd35ccecb50b6b0fb0bd9eb481f
freek <freek@spatie.be>
2019-12-13 16:22:09 +0100

    wip

--- config/mailcoach.php
+++ config/mailcoach.php
@@ -26,7 +26,7 @@ return [
         'enabled' => true,
         'redis_connection_name' => 'default',
         'redis_key' => 'laravel-mailcoach',
-        'allowed_number_of_jobs_in_timespan' => 1,
+        'allowed_number_of_jobs_in_timespan' => 2,
         'timespan_in_seconds' => 1,
         'release_in_seconds' => 5,
     ],
