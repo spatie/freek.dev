13a7caf727496ac9ef6442100e7db78404b3d867
freek <freek@spatie.be>
2019-12-23 17:20:31 +0100

    wip

--- resources/views/front/newsletter/partials/form.blade.php
+++ resources/views/front/newsletter/partials/form.blade.php
@@ -5,7 +5,6 @@
     class="flex flex-col md:flex-row items-stretch {{ $class ?? '' }}"
 >
     @csrf
-    @honeypot
     <input type="email" autocomplete="off" id="email" name="email" placeholder="Your e-mail address" class="flex-1 px-3 py-2 bg-white focus:outline-none focus:border-gray-400 border-y-3 border-t-transparent mb-2 md:mb-0" aria-label="E-mail" required>
 
     <input type="submit" name="submit" id="submit" value="Subscribe" class="px-3 py-2 text-sm text-white bg-orange-500 font-semibold border-y-3 border-orange-700 border-t-transparent">
