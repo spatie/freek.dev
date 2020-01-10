75eef7e68b8117c673eb57f9e6aad12b4831f61e
freek <freek@spatie.be>
2020-01-10 17:01:18 +0100

    wip

--- config/mailcoach.php
+++ config/mailcoach.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     'throttling' => [
         'enabled' => true,
         'redis_connection_name' => 'default',
@@ -9,5 +8,9 @@ return [
         'allowed_number_of_jobs_in_timespan' => 10,
         'timespan_in_seconds' => 1,
         'release_in_seconds' => 5,
+    ],
+
+    'mailgun_feedback' => [
+        'signing_secret' => env('MAILGUN_SIGNING_SECRET'),
     ],
 ];
