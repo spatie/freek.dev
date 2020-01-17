b66fa1c85cc6e12ec34fdba34bf86ea762ad322c
freek <freek@spatie.be>
2020-01-17 08:48:47 +0100

    wip

--- resources/views/front/links/create.blade.php
+++ resources/views/front/links/create.blade.php
@@ -5,40 +5,44 @@
 
 @section('content')
 
-    <form method="POST">
-        @csrf
-        <div>
-            <label for="title">Title</label>
-            <input name="title" type="text" required value="{{ old('title') }}">
-            @error('title')
-            <span role="alert">
+    <div class="markup | mb-8">
+        <h1>Submit a link</h1>
+
+        <form method="POST">
+            @csrf
+            <div>
+                <label for="title">Title</label>
+                <input name="title" type="text" required value="{{ old('title') }}">
+                @error('title')
+                <span role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
-            @enderror
-        </div>
+                @enderror
+            </div>
 
-        <div>
-            <label for="url">Url</label>
-            <input name="url" type="url" required value="{{ old('url') }}">
-            @error('url')
-            <span role="alert">
+            <div>
+                <label for="url">Url</label>
+                <input name="url" type="url" required value="{{ old('url') }}">
+                @error('url')
+                <span role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
-            @enderror
-        </div>
+                @enderror
+            </div>
 
-        <div>
-            <label for="text">Description</label>
-            <textarea name="text" type="text" required>
+            <div>
+                <label for="text">Description</label>
+                <textarea name="text" type="text" required>
                 {{ old('text') }}
             </textarea>
-            @error('text')
-            <span role="alert">
+                @error('text')
+                <span role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
-            @enderror
-        </div>
+                @enderror
+            </div>
 
-        <button type="submit">Submit</button>
-    </form>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
 @endsection
