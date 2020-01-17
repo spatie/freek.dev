bb9a32d5750b8bc1ac312f92fda3417abd8c2038
freek <freek@spatie.be>
2020-01-17 10:06:07 +0100

    add-links-section

--- resources/views/front/links/create.blade.php
+++ resources/views/front/links/create.blade.php
@@ -5,44 +5,57 @@
 
 @section('content')
 
-    <div class="markup | mb-8">
+    <div class="markup mb-8">
         <h1>Submit a link</h1>
 
         <form method="POST">
             @csrf
-            <div>
-                <label for="title">Title</label>
-                <input  name="title" type="text" required value="{{ old('title') }}">
+            <div class="mt-4">
+                <label class="text-gray-700" for="title">
+                    <span class="text-gray-700">Title</span>
+                    <input class="form-input mt-1 block w-full" name="title" type="text" value="{{ old('title') }}">
+                </label>
                 @error('title')
-                <span role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
+                <div
+                    class="mt-2 py-2 px-2 flex-1 bg-red-500 focus:outline-none md:mb-0 text-white text-2xs">{{ $message }}</div>
                 @enderror
             </div>
 
-            <div>
-                <label for="url">Url</label>
-                <input name="url" type="url" required value="{{ old('url') }}">
+            <div class="mt-4">
+                <label class="text-gray-700" for="url">
+                    <span class="text-gray-700">URL</span>
+                    <input class="form-input mt-1 block w-full" name="url" type="text" required
+                           value="{{ old('title') }}"
+                           placeholder="https://"
+                    >
+                </label>
                 @error('url')
-                <span role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
+                <div
+                    class="mt-2 py-2 px-2 flex-1 bg-red-500 focus:outline-none md:mb-0 text-white text-2xs">{{ $message }}</div>
+
                 @enderror
             </div>
 
-            <div>
-                <label for="text">Description</label>
-                <textarea name="text" type="text" required>
-                {{ old('text') }}
-            </textarea>
+            <div class="mt-4">
+                <label class="text-gray-700" for="text">
+                    <span class="text-gray-700">Description</span>
+                    <textarea
+                        placeholder="Describe the content in one or two sentences"
+                        class="form-textarea mt-1 block w-full" name="text" type="text"
+                              required>{{ old('text') }}</textarea>
+                </label>
                 @error('text')
-                <span role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
+                <div
+                    class="mt-2 py-2 px-2 flex-1 bg-red-500 focus:outline-none md:mb-0 text-white text-2xs">{{ $message }}</div>
                 @enderror
             </div>
 
-            <button type="submit">Submit</button>
+            <div class="mt-4">
+                <button
+                    class="button button-orange"
+                    type="submit">Submit
+                </button>
+            </div>
         </form>
     </div>
 @endsection
