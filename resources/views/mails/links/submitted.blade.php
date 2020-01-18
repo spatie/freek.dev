8751ca1043abb397c20fee65af102d8b8cff199f
freek <freek@spatie.be>
2020-01-18 12:01:47 +0100

    wip

--- resources/views/mails/links/submitted.blade.php
+++ resources/views/mails/links/submitted.blade.php
@@ -1,10 +1,14 @@
 @component('mail::message')
 Hi,
 
-A link titled "[{{ $link->title }}]($link->url)" was submitted by {{ $link->user->email }}. You can approve or reject this link [here](/nova/resources/links/{{ $link->id }}).
+A link titled "[{{ $link->title }}]($link->url)" was submitted by {{ $link->user->email }}.
+
+{{ $link->text }}
+
+You can approve or reject this link [here](/nova/resources/links/{{ $link->id }}).
+
 
 Kr,
-
 
 
 Your blog
