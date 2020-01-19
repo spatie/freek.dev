ddf552c45f0ad46f24be1c9b173838ea97daaa5d
freek <freek@spatie.be>
2020-01-19 15:36:49 +0100

    wip

--- resources/views/mails/links/approved.blade.php
+++ resources/views/mails/links/approved.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 Hi,
 
-Your link [{{ $link->title }}]($link->url) was approved. You can now view it on [the links page at freek.dev]({{ route('links') }})
+Your link [{{ $link->title }}]({{ $link->url }}) was approved. You can now view it on [the links page at freek.dev]({{ route('links') }})
 
 Thanks,
 
