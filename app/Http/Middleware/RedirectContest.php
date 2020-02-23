c8a57af64af1003dfa2ec0823e1dd8fec58e1a9d
freek <freek@spatie.be>
2020-02-23 18:29:50 +0100

    wip

--- app/Http/Middleware/RedirectContest.php
+++ app/Http/Middleware/RedirectContest.php
@@ -16,7 +16,7 @@ class RedirectContest
      */
     public function handle($request, Closure $next)
     {
-        info($request->segment(0), $request->segment(1));
+        info($request->segment(1));
         if ($request->get('utm_campaign') === 'referral') {
             if ($request->segment(0) !== 'mailcoach-contest') {
                 info($request->segment(0));
