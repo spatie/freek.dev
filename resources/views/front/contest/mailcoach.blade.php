8618982e23d0857bcb1754e7f301599b563c8438
freek <freek@spatie.be>
2020-02-23 14:58:36 +0100

    wip

--- resources/views/front/contest/mailcoach.blade.php
+++ resources/views/front/contest/mailcoach.blade.php
@@ -5,21 +5,23 @@
 
 @section('content')
     <div class="markup mb-4">
-        <h1>Contest</h1>
+        <h1>Win a free Mailcoach license</h1>
+
+        <p>
+            <a href="https://mailcoach.app">Mailcoach</a> is a self-hosted email list manager. It integrates with services like Amazon SES, Mailgun or Sendgrid to send out mailings affordably.
+            Stand-alone, or integrated in a Laravel project, it's perfect for bloggers, artisans and entrepreneurs.
+        </p>
+        <p>
+            A mailcoach license also give you access to <a href="https://mailcoach.app/videos">the video course</a>. Get a look behind the scenes of Mailcoach, a real world Laravel application for newsletters. Learn about the problems that we tackled and the clean code patterns that we applied.
+        </p>
+        <p>
+            We've partnered with <a href="https://contestkit.com">ContestKit</a> to give away three Mailcoach licenses for free.
+        </p>
+
+        <a href="#" id="6863w5qN"></a>
+        <script src="https://embed.contestkit.com/iframe/6863w5qN" data-ck-6863w5qN data-ck-element-id="6863w5qN"></script>
     </div>
 
-    <p>
-        <a href="https://mailcoach.app">Mailcoach</a> is a self-hosted email list manager. It integrates with services like Amazon SES, Mailgun or Sendgrid to send out mailings affordably.
-        Stand-alone, or integrated in a Laravel project, it's perfect for bloggers, artisans and entrepreneurs.
-    </p>
-    <p>
-        A mailcoach license also give you access to <a href="https://mailcoach.app/videos">the video course</a>. Get a look behind the scenes of Mailcoach, a real world Laravel application for newsletters. Learn about the problems that we tackled and the clean code patterns that we applied.
-    </p>
-    <p>
-        We've partnered with <a href="https://contestkit.com">ContestKit</a> to give away three Mailcoach licenses for free.
-    </p>
 
-    <a href="#" id="6863w5qN"></a>
-    <script src="https://embed.contestkit.com/iframe/6863w5qN" data-ck-6863w5qN data-ck-element-id="6863w5qN"></script>
 
 @endsection
