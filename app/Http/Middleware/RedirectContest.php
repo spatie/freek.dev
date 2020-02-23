0d5a9874206102913d4dfcd441da70ab9b5df834
freek <freek@spatie.be>
2020-02-23 17:53:54 +0100

    wip

--- app/Http/Middleware/RedirectContest.php
+++ app/Http/Middleware/RedirectContest.php
@@ -17,7 +17,7 @@ class RedirectContest
     {
         if ($request->get('utm_campaign') === 'referral') {
             // if ($request->segment(1) !== 'mailcoach-contest') {
-            return redirect()->to('/mailcoach-contest?referral=QWkWqO9M&utm_campaign=referral');
+            return redirect()->to('/mailcoach-contest');
             //  }
         }
 
