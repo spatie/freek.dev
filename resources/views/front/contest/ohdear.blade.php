445579f4054b2cdb57d44ab01cb71b9deb1b3f9a
freek <freek@spatie.be>
2020-02-24 09:14:06 +0100

    wip

--- resources/views/front/contest/ohdear.blade.php
+++ resources/views/front/contest/ohdear.blade.php
@@ -7,7 +7,7 @@
     <div class="markup mb-4">
         <h1>Monitor 2 sites for free for an entire year using Oh Dear</h1>
         <p>
-            <a href="https://ohdear.app">Oh Dear</a> monitors uptime, checks your SSL certificates, crawls for broken links and sends out notifications when something's wrong. It checks your entire website, not just the homepage.  When something is wrong Oh Dear can notify you via Slack, SMS, Pushover, ...  we can notify you wherever your team is active. It has a developer friendly API and [kick-ass docs](https://ohdear.app/docs).
+            <a href="https://ohdear.app">Oh Dear</a> monitors uptime, checks your SSL certificates, crawls for broken links and sends out notifications when something's wrong. It checks your entire website, not just the homepage.  When something is wrong Oh Dear can notify you via Slack, SMS, Pushover, ...  we can notify you wherever your team is active. It has a developer friendly API and <a href="https://ohdear.app/docs">kick-ass docs</a>.
         </p>
         <p>
             In times of crisis, a public status page allows you to communicate to your clients. We'll host your status page so it's always available. You can focus on providing updates to your users. Here are the Oh Dear powered status pages for <a href="https://status.laravel.com">Laravel</a> and <a href="https://status.flareapp.io">Flare</a>.
