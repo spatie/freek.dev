ae35cb198cc03ef4ae27d087889505fe44c343f3
Laravel Shift <shift@laravelshift.com>
2020-03-08 18:54:44 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them so you can review the commit diff for
    changes. Moving forward, you should use ENV variables or create a
    separate config file to allow the core config files to remain
    automatically upgradeable.

--- config/logging.php
+++ config/logging.php
@@ -1,5 +1,6 @@
 <?php
 
+use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
 
@@ -36,12 +37,8 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily', 'flare'],
+            'channels' => ['single'],
             'ignore_exceptions' => false,
-        ],
-
-        'flare' => [
-            'driver' => 'flare',
         ],
 
         'single' => [
@@ -92,6 +89,15 @@ return [
         'errorlog' => [
             'driver' => 'errorlog',
             'level' => 'debug',
+        ],
+
+        'null' => [
+            'driver' => 'monolog',
+            'handler' => NullHandler::class,
+        ],
+
+        'emergency' => [
+            'path' => storage_path('logs/laravel.log'),
         ],
     ],
 
