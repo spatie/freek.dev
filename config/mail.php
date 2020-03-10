2c8356776d7e6b333d455b0eaf607b1efe971633
freek <freek@spatie.be>
2020-03-10 23:09:29 +0100

    wip

--- config/mail.php
+++ config/mail.php
@@ -82,8 +82,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => 'freek@spatie.be',
+        'name' => 'Freek Van der Herten',
     ],
 
     /*
