6e241e7e5378b19de2acf8795167dbb2a3f625ec
freek <freek@spatie.be>
2020-05-28 15:36:30 +0200

    wip

--- resources/views/front/contest/laravel-package-training.blade.php
+++ resources/views/front/contest/laravel-package-training.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout title="Laravel Package Training Video Course">
 
     <div class="markup mb-4">
-        <h1>Win the Laravel Package Training video training for free</h1>
+        <h1>Watch the Laravel Package Training video training for free</h1>
 
         <a href="#" id="kvNYB4E"></a>
         <script src="https://e.cnt.st/iframe/kvNYB4E" data-ck-kvNYB4E></script>
