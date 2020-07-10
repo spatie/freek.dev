bebbc8a9feb14dab1ac21b699d1cef3e1f8b9b1d
freek <freek@spatie.be>
2020-07-10 20:34:44 +0200

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('mailcoach:send-campaign-summary-mail')->hourly();
         $schedule->command('mailcoach:send-email-list-summary-mail ')->mondays()->at('9:00');
         $schedule->command('mailcoach:delete-old-unconfirmed-subscribers')->daily();
+        $schedule->command('schedule-monitor:clean')->monthly();
     }
 
     protected function commands()
