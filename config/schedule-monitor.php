e048e3cb8a49cca19a01b9bb5cf54be4320d365d
freek <freek@spatie.be>
2020-07-26 20:57:23 +0200

    wip

--- config/schedule-monitor.php
+++ config/schedule-monitor.php
@@ -6,7 +6,7 @@ return [
      * After a while the `monitored_scheduled_task_log_items` might become big.
      * Here you can specify the amount of days log items should be kept.
      */
-    'delete_log_items_older_than_days' => 30,
+    'delete_log_items_older_than_days' => 7,
 
     /*
      * Oh Dear can notify you via Mail, Slack, SMS, web hooks, ... when a
