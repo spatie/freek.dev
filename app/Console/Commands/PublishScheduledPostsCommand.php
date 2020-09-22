eca2b17877e7d007025f8ff007acd68493beb348
freek <freek@spatie.be>
2020-09-22 10:49:04 +0200

    wip

--- app/Console/Commands/PublishScheduledPostsCommand.php
+++ app/Console/Commands/PublishScheduledPostsCommand.php
@@ -14,6 +14,7 @@ class PublishScheduledPostsCommand extends Command
 
     public function handle(PublishPostAction $publishPostAction)
     {
+        dd('nope');
         Post::scheduled()->get()
             ->reject(fn (Post $post) => $post->publish_date->isFuture())
             ->each(fn (Post $post) => $publishPostAction->execute($post));
