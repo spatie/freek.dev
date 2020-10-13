3f2a1f02b9a681ef87aa113c770642d3195fac7f
freek <freek@spatie.be>
2020-10-13 10:08:34 +0200

    wip

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -110,7 +110,7 @@ class Post extends Model implements Feedable, Sluggable, HasMedia
         if ($this->isPartOfSeries()) {
             $text = str_replace(
                 ['[spatie-series-toc]', '[series-toc]'],
-                (new SeriesTocComponent($this))->render(),
+                (new SeriesTocComponent($this))->render() . PHP_EOL,
                 $this->text
             );
 
