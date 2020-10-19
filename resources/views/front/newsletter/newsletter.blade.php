053d54e51f701e4280c5099f39cf849f0ecd4232
freek <freek@spatie.be>
2020-10-19 23:04:50 +0200

    wip

--- resources/views/front/newsletter/newsletter.blade.php
+++ resources/views/front/newsletter/newsletter.blade.php
@@ -1,3 +1,6 @@
-
-
-{!! $campaign->webview_html !!}
+<div>
+    <div style="position:relative;padding-top:56.25%;">
+        <iframe src="{{ $campaign->webViewUrl() }}" frameborder="0" allowfullscreen
+                style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+    </div>
+</div>
