cab1c4c15026980494c5076d763b6bf233ce03dd
freek <freek@spatie.be>
2020-10-19 23:01:06 +0200

    wip

--- resources/views/front/newsletter/newsletter.blade.php
+++ resources/views/front/newsletter/newsletter.blade.php
@@ -1,12 +1,3 @@
-<html lang="">
-<head>
-    <title>{{ $campaign->subject }}</title>
-    <style>{!! file_get_contents(public_path('css/app.css')) !!}</style>
-    <link rel="stylesheet" href="https://cloud.typography.com/6194432/6581412/css/fonts.css"/>
-</head>
-<body>
-@include('front.newsletter.partials.block')
+
 
 {!! $campaign->webview_html !!}
-</body>
-</html>
