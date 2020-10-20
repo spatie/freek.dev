c4641abfd57b6bf7267438f17693d91817bfdea4
freek <freek@spatie.be>
2020-10-20 08:37:58 +0200

    wip

--- resources/views/front/newsletter/newsletter.blade.php
+++ resources/views/front/newsletter/newsletter.blade.php
@@ -21,11 +21,7 @@
     </div>
 </div>
 
-<div>
-    <div style="position:relative;padding-top:56.25%;">
-        <iframe src="{{ $campaign->webViewUrl() }}" frameborder="0" allowfullscreen
-                style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
-    </div>
-</div>
+{{-- justify-center --}}
+        <iframe src="https://httpstat.us" class="w-screen h-screen"></iframe>
 </body>
 </html>
