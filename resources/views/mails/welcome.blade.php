d41ba64fdd40bae6930e2dc6551030a53881f3df
freek <freek@spatie.be>
2020-11-06 10:59:07 +0100

    wip

--- resources/views/mails/welcome.blade.php
+++ resources/views/mails/welcome.blade.php
@@ -23,7 +23,7 @@ Here are some blog posts I recently published on [freek.dev](https://freek.dev):
 - [{{ $post->title }}]({{ $post->url }})
 @endforeach
 
-By clicking [this link](https://twitter.com/intent/tweet?url=https%3A%2F%2Ffreek.dev%2Fnewsletter&text=I%27ve%20just%20signed%20up%20for%20the%20newsletter%20on%20Laravel%2C%20PHP%20and%20JavaScript%20by%20@freekmurze.%20You%20can%20sign%20up%20via%20this%20link%3A&hashtags=laravel%2C%20php%2C%20javascript) you can share your subscription with your followers on Twitter.
+By clicking [this link](https://twitter.com/intent/tweet?url=https%3A%2F%2Ffreek.dev%2Fnewsletter&text=I%27ve%20just%20signed%20up%20for%20the%20newsletter%20on%20Laravel%2C%20PHP%20and%20JavaScript%20by%20@freekmurze.%20You%20can%20sign%20up%20via%20this%20link%3A&hashtags=laravel%2Cphp%2Cjavascript) you can share your subscription with your followers on Twitter.
 
 Thanks for subscribing!
 
