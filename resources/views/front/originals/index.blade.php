b8b831931766c64d97ceb096b28343f33f27ce72
freek <freek@spatie.be>
2020-12-04 22:14:27 +0100

    wip

--- resources/views/front/originals/index.blade.php
+++ resources/views/front/originals/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout title="Originals">
     <div
-        class="-mx-4 sm:mx-0 p-4 sm:p-6 md:p-8 mb-6 bg-gray-100 border-b-5 border-grey-200 text-sm text-gray-700">
+        class="-mx-4 sm:mx-0 p-4 sm:p-6 md:p-8 mb-8 bg-gray-100 border-b-5 border-grey-200 text-sm text-gray-700">
 
         <p>
             In this section you can read posts I've written myself.
