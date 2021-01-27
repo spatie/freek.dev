5fe855cc82667e401c4841df161e733d8199bc08
freek <freek@spatie.be>
2021-01-27 13:38:18 +0100

    wip

--- resources/views/mails/welcome.blade.php
+++ resources/views/mails/welcome.blade.php
@@ -5,6 +5,7 @@ Welcome to the freek.dev newsletter.  Every two weeks you can expect some nice l
 
 To thank you for subscribing I'd like to offer you a coupon which grants you a discount when purchasing one of these products of mine:
 
+- [Ray](https://myray.app) is a tool that help you to debug faster. It's as easy as dump-and-die debugging but way more powerful. I'm using this myself in all projects I work on.
 - [Oh Dear](https://ohdear.app) will notify you whenever your website goes down. Unlike most other services, Oh Dear does not only monitor your homepage, but your entire website. It can notify you when you have broken links or mixed content. It will also monitor the performance of your site, your SSL certificates, provides public status page like [this one](https://status.laravel.com), and much more. The coupon offers you a discount of 5 EUR on your first bill.
 - In the [Laravel Package Training video course](https://laravelpackage.training) you'll learn how to create quality framework agnostic PHP and Laravel packages. We'll also source dive a couple of my packages together. I'm sure you will pick up some coding tricks! The coupon grants you a 5% discount.
 - [Mailcoach](https://mailcoach.app) is a premium Laravel package that can help sending email campaigns of any size in an affordable way. The coupon grants you a 5% discount.
