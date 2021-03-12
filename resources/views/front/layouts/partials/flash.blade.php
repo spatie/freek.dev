5891e8827814dde6dc9fedadcbdd89d3f5348852
freek <freek@spatie.be>
2021-03-12 12:19:24 +0100

    wip

--- resources/views/front/layouts/partials/flash.blade.php
+++ resources/views/front/layouts/partials/flash.blade.php
@@ -1,14 +1,15 @@
 @if(flash()->message)
-    <div class="{{ flash()->class }}">{{ flash()->message }}</div>
+    <div
+        class="text-white font-bold { flash()->class }}  p-2 {text-center"
+        x-data="{show: true}"
+        x-show.transition.duration.1000ms="show"
+
+        x-init="setTimeout(() => show = false, 8000)"
+    >
+        {{ flash()->message }}
+    </div>
 @endif
 
-<div
-    class="text-white font-bold bg-green-400 p-2 {{ flash()->class }} text-center"
-    x-data="{show: true}"
-    x-show.transition.duration.1000ms="show"
 
-    x-init="setTimeout(() => show = false, 8000)"
->
-    here
-</div>
+
 
