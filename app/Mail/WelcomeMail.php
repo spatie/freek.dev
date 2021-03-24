614157000615dac343c16bc537edceada0388233
freek <freek@spatie.be>
2021-03-24 13:50:40 +0100

    wip

--- app/Mail/WelcomeMail.php
+++ app/Mail/WelcomeMail.php
@@ -2,11 +2,7 @@
 
 namespace App\Mail;
 
-use App\Models\Post;
-use Illuminate\Support\Collection;
-use Spatie\Mailcoach\Domain\Audience\Models\Subscriber;
 use Spatie\Mailcoach\Domain\Campaign\Mails\WelcomeMail as MailcoachWelcomeMail;
-use Spatie\Mailcoach\Domain\Campaign\Models\Campaign;
 use Spatie\Mailcoach\Domain\TransactionalMail\Mails\Concerns\StoresMail;
 use Spatie\Mailcoach\Domain\TransactionalMail\Mails\Concerns\UsesMailcoachTemplate;
 
@@ -15,23 +11,8 @@ class WelcomeMail extends MailcoachWelcomeMail
     use StoresMail;
     use UsesMailcoachTemplate;
 
-    public Collection $posts;
-
-    public Collection $campaigns;
-
-    public function __construct(Subscriber $subscriber)
-    {
-        parent::__construct($subscriber);
-
-        $this->campaigns = Campaign::sent()->orderByDesc('sent_at')->limit(3)->get();
-
-        $this->posts = Post::published()->originalContent()->orderByDesc('publish_date')->limit(10)->get();
-    }
-
     public function build()
     {
-        return
-            $this
-                ->template('welcome');
+        return $this->template('welcome');
     }
 }
