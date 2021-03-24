312f93a6df0c42c339e46d771c74b6e1b077cd4a
freek <freek@spatie.be>
2021-03-24 13:52:29 +0100

    wip

--- app/Mail/WelcomeMail.php
+++ app/Mail/WelcomeMail.php
@@ -3,12 +3,10 @@
 namespace App\Mail;
 
 use Spatie\Mailcoach\Domain\Campaign\Mails\WelcomeMail as MailcoachWelcomeMail;
-use Spatie\Mailcoach\Domain\TransactionalMail\Mails\Concerns\StoresMail;
 use Spatie\Mailcoach\Domain\TransactionalMail\Mails\Concerns\UsesMailcoachTemplate;
 
 class WelcomeMail extends MailcoachWelcomeMail
 {
-    use StoresMail;
     use UsesMailcoachTemplate;
 
     public function build()
