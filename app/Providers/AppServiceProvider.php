bcbac1f07740ac0b4120ef79d9216fa9c170e59a
freek <freek@spatie.be>
2021-03-31 13:58:28 +0200

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Facade\Ignition\Facades\Flare;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Gate;
@@ -18,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
 
         Gate::define('viewMailcoach', function (User $user) {
             return $user->email === 'freek@spatie.be';
+        });
+
+        Flare::determineVersionUsing(function() {
+           return '1.0';
         });
 
         Carbon::setToStringFormat('jS F Y');
