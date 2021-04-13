ee8c73b7a4ca9349990c687d4e98d8c652cc3d45
freek <freek@spatie.be>
2021-04-13 14:24:44 +0200

    add field

---
+++ database/migrations/2021_04_13_142256_add_generated_conversions_field_to_media_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class AddGeneratedConversionsFieldToMediaTable extends Migration {
+    public function up() {
+        if ( ! Schema::hasColumn( 'media', 'generated_conversions' ) ) {
+            Schema::table( 'media', function ( Blueprint $table ) {
+                $table->json( 'generated_conversions' );
+            } );
+        }
+
+        Media::query()
+            ->whereNull('generated_conversions')
+            ->orWhere('generated_conversions', '')
+            ->orWhereRaw("JSON_TYPE(generated_conversions) = 'NULL'")
+            ->update([
+                'generated_conversions' => DB::raw('custom_properties->"$.generated_conversions"'),
+            ]);
+    }
+}
