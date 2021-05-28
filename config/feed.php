88b313fac7bdaae53cf243abac5b2016117c031e
freek <freek@spatie.be>
2021-05-28 13:07:38 +0200

    wip

--- config/feed.php
+++ config/feed.php
@@ -4,19 +4,19 @@ return [
 
     'feeds' => [
         'main' => [
-            'url' => '/feed',
+            'url' => '/',
             'title' => 'freek.dev - all blogposts',
             'items' => \App\Models\Post::class . '@getFeedItems',
         ],
 
         'php' => [
-            'url' => '/feed/php',
+            'url' => '/php',
             'title' => 'freek.dev - all php blogposts',
             'items' => \App\Models\Post::class . '@getPhpFeedItems',
         ],
 
         'originals' => [
-            'url' => '/feed/originals',
+            'url' => '/originals',
             'title' => 'freek.dev - all originally written blogposts',
             'items' => \App\Models\Post::class . '@getOriginalContentFeedItems',
         ],
