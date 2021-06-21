748c2a7b21464c9c42cb98d1249d07f4d5e3ac11
Rias <hey@rias.be>
2021-06-21 16:52:36 +0200

    Update scope.js

--- public/scope.js
+++ public/scope.js
@@ -1,15 +1,73 @@
-var url = (new URL(document.location));
-var params = url.searchParams;
+(function() {
+    'use strict';
+    var location = window.location
+    var document = window.document
+    var element = document.currentScript;
+    var uuid = 'f23c42d8-2715-4498-b7ca-a3bc544650b4';
+    var endpoint = 'https://scope.spatie.be/scope';
 
-var xhr = new XMLHttpRequest();
-xhr.open("POST", 'https://scope.spatie.be/scope', true);
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.send(JSON.stringify({
-    url: window.location.href,
-    screen_size: window.innerWidth,
-    referrer: document.referrer,
-    utm_campaign: params.get('utm_campaign'),
-    utm_source: params.get('utm_source'),
-    utm_medium: params.get('utm_medium'),
-    site_uuid: 'f23c42d8-2715-4498-b7ca-a3bc544650b4',
-}));
+    var url = (new URL(location));
+    var params = url.searchParams;
+    var lastPage;
+
+    function trigger(eventName, options) {
+        if (/^localhost$|^127(?:\.[0-9]+){0,2}\.[0-9]+$|^(?:0*\:)*?:?0*1$/.test(location.hostname) || location.protocol === 'file:') return;
+
+        var payload = {}
+        payload.site_uuid = uuid;
+        payload.url = location.href;
+        payload.name = eventName;
+        payload.screen_size = window.innerWidth;
+        payload.referrer = document.referrer || null;
+        payload.utm_campaign = params.get('utm_campaign');
+        payload.utm_source = params.get('utm_source');
+        payload.utm_medium = params.get('utm_medium');
+
+        if (options && options.meta) {
+            payload.meta = JSON.stringify(options.meta)
+        }
+
+        var request = new XMLHttpRequest();
+        request.open('POST', endpoint, true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify(payload));
+
+        request.onreadystatechange = function() {
+            if (request.readyState == 4) {
+                options && options.callback && options.callback();
+            }
+        }
+    }
+
+    function page() {
+        if (lastPage === location.pathname) return;
+        lastPage = location.pathname
+        trigger('visit');
+    }
+
+    function handleVisibilityChange() {
+        if (!lastPage && document.visibilityState === 'visible') {
+            page();
+        }
+    }
+
+    var history = window.history;
+    if (history.pushState) {
+        var originalPushState = history['pushState'];
+
+        history.pushState = function() {
+            originalPushState.apply(this, arguments)
+            page();
+        }
+
+        window.addEventListener('popstate', page);
+    }
+
+    window.scope = trigger;
+
+    if (document.visibilityState === 'prerender') {
+        document.addEventListener("visibilitychange", handleVisibilityChange);
+    } else {
+        page();
+    }
+})();
