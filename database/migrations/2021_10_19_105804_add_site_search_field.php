72cf76a9b2055db6fe14872f9d7a1bb377f207bf
freek <freek@spatie.be>
2021-10-19 11:02:07 +0200

    wip

---
+++ database/migrations/2021_10_19_105804_add_site_search_field.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('site_search_configs', function(Blueprint $table) {
+            $table->integer('number_of_urls_indexed')->default(0);
+        });
+    }
+};
