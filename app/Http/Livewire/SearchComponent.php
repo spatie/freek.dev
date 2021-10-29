4f2deed181e04bca409cac193e57c99b00da831f
freek <freek@spatie.be>
2021-10-29 09:58:22 +0200

    wip

--- app/Http/Livewire/SearchComponent.php
+++ app/Http/Livewire/SearchComponent.php
@@ -2,13 +2,9 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\Post;
-use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Collection;
 use Livewire\Component;
-use Livewire\WithPagination;
 use Spatie\SiteSearch\Search;
-use Spatie\SiteSearch\SearchIndexQuery;
 
 class SearchComponent extends Component
 {
@@ -16,8 +12,6 @@ class SearchComponent extends Component
 
     public function render()
     {
-        info($this->query);
-        info(count($this->getResults()) . ' hits');
         return view('livewire.search', [
             'hits' => $this->getResults(),
         ]);
