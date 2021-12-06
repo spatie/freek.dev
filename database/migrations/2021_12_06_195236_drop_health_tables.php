7edd486dd1bc08fbc63fb7096ef03479a2e94019
freek <freek@spatie.be>
2021-12-06 19:52:59 +0100

    wip

---
+++ database/migrations/2021_12_06_195236_drop_health_tables.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::drop('health_check_result_history_items');
+    }
+};
