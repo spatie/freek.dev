d16a9919417fd8d48c91aa9a02ac00b5817b1147
freek <freek@spatie.be>
2021-12-14 15:23:51 +0100

    wip

--- app/Providers/HealthServiceProvider.php
+++ app/Providers/HealthServiceProvider.php
@@ -3,7 +3,13 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Spatie\CpuLoadHealthCheck\CpuLoadCheck;
+use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\DebugModeCheck;
 use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\FlareErrorOccurrenceCountCheck;
+use Spatie\Health\Checks\Checks\HorizonCheck;
+use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
 use Spatie\Health\Facades\Health;
 
 class HealthServiceProvider extends ServiceProvider
@@ -11,7 +17,18 @@ class HealthServiceProvider extends ServiceProvider
     public function register()
     {
         Health::checks([
+            CpuLoadCheck::new()->failWhenLoadIsHigherInTheLast5Minutes(2.0),
+            DebugModeCheck::new(),
             EnvironmentCheck::new(),
+            DatabaseCheck::new(),
+            HorizonCheck::new(),
+            UsedDiskSpaceCheck::new()
+                ->warnWhenUsedSpaceIsAbovePercentage(90)
+                ->failWhenUsedSpaceIsAbovePercentage(95),
+            FlareErrorOccurrenceCountCheck::new()
+                ->projectId(config('services.flare.project_id'))
+                ->apiToken(config('services.flare.api_token'))
+                ->failWhenMoreErrorsReceivedThan(300)
         ]);
     }
 }
