8f7bc8cb243c04b8942253f1995342eaea87b11c
freek <freek@spatie.be>
2021-12-14 11:24:48 +0100

    wip

--- app/Providers/HealthServiceProvider.php
+++ app/Providers/HealthServiceProvider.php
@@ -3,11 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Facades\Health;
 
 class HealthServiceProvider extends ServiceProvider
 {
     public function register()
     {
-
+        Health::checks([
+           EnvironmentCheck::new(),
+        ]);
     }
 }
