1375e4fca0b79c6a784b8a8bacc20e469bb5c856
freek <freek@spatie.be>
2021-12-14 11:13:17 +0100

    wip

---
+++ app/Providers/HealthServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HealthServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+}
