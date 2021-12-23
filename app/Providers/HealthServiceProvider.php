97e324ecc9655ca5a5bf45d827ed3aad80ed178a
freek <freek@spatie.be>
2021-12-24 00:40:43 +0100

    wip

--- app/Providers/HealthServiceProvider.php
+++ app/Providers/HealthServiceProvider.php
@@ -20,7 +20,7 @@ class HealthServiceProvider extends ServiceProvider
     public function register()
     {
         Health::checks([
-            CpuLoadCheck::new()->failWhenLoadIsHigherInTheLast5Minutes(3.0),
+            CpuLoadCheck::new()->failWhenLoadIsHigherInTheLast5Minutes(4.0),
             DebugModeCheck::new(),
             MeiliSearchCheck::new(),
             EnvironmentCheck::new(),
