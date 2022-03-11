6ba9b491a2063a0d1804c8cdc04c430707edae38
Freek Van der Herten <freek@spatie.be>
2022-03-11 13:37:21 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,9 @@ use Spatie\RouteDiscovery\Discovery\Discover;
 
 Discover::views()->in(resource_path('views/discovery'));
 Discover::controllers()->in(app_path('Http/Controllers/Discovery'));
+
+Route::get('ray', function() {
+   ray('Greetings from the server');
+
+   return 'hello there';
+});
