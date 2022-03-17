6b9e817f1da9c1fd15ea66ec10e3aea81247d3ba
Freek Van der Herten <freek@spatie.be>
2022-03-17 11:25:10 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ use Spatie\RouteDiscovery\Discovery\Discover;
 
 Route::redirect('nova', '/nova/resources/posts');
 
+Route::get('debug', function() {
+   ray('greetings from your server');
+
+   return 'ok!';
+});
+
 Discover::views()->in(resource_path('views/discovery'));
 Discover::controllers()->in(app_path('Http/Controllers/Discovery'));
 
