2f7955cf811e15a87779227cd9aa7d45c7684cb7
Freek Van der Herten <freek@spatie.be>
2022-04-27 14:19:37 +0200

    wip

---
+++ resources/views/discovery/test-mailcoach.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<body>
+<form method="POST" action="https://php-foundation-mailcoach.com/mailcoach/subscribe/9be4e2bd-f9d8-475c-b00e-2dcc4cf90056">
+    <div>
+        <label for="email">Email</label>
+        <input name="email">
+    </div>
+    <div>
+        <button type="submit">Subscribe</button>
+    </div>
+</form>
+</body>
+</html>
