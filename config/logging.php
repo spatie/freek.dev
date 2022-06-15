b432f3753348a85473b6faaf778047a1686e7040
Freek Van der Herten <freek@spatie.be>
2022-06-15 10:07:33 +0200

    wip

--- config/logging.php
+++ config/logging.php
@@ -50,7 +50,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single', 'flare'],
+            'channels' => ['daily', 'flare'],
             'ignore_exceptions' => false,
         ],
 
