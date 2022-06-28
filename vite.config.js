ce90825fefaf3c437cf958e949db01d563675749
Freek Van der Herten <freek@spatie.be>
2022-06-28 23:20:12 +0200

    hot reload blade

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,17 @@ export default defineConfig({
         laravel([
             'resources/js/app.js',
         ]),
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                }
+            },
+        }
     ],
     server: detectServerConfig(host),
 })
