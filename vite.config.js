a56cb33f3257bd9ab286aa6c7b1fd201145addfe
Freek Van der Herten <freek@spatie.be>
2022-06-28 18:00:19 +0200

    wip

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
+    server: {
+        host: 'freek.dev.test'
+    },
     plugins: [
         laravel([
             'resources/css/app.css',
