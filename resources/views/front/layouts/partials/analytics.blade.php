6e2f4ee04c6c9d91710e50f5288aacbc0d0c602e
Freek Van der Herten <freek@spatie.be>
2022-07-05 00:17:02 +0200

    wip

--- resources/views/front/layouts/partials/analytics.blade.php
+++ resources/views/front/layouts/partials/analytics.blade.php
@@ -1,4 +1,6 @@
 @if(app()->environment('production'))
+    <script src="https://cdn.usefathom.com/script.js" data-site="FBTOHQOK" defer></script>
+
     <script async="async" src="https://www.googletagmanager.com/gtag/js?id=UA-57290920-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
