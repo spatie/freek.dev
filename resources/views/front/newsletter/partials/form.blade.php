f104a37a58fc819f529b91c4db072574f78d336f
Rias <hey@rias.be>
2022-07-18 10:20:12 +0200

    Use form submission instead (#127)

--- resources/views/front/newsletter/partials/form.blade.php
+++ resources/views/front/newsletter/partials/form.blade.php
@@ -1,5 +1,5 @@
 <form
-    action="{{ route('newsletter.subscribe') }}"
+    action="https://freek-dev.mailcoach.app/subscribe/590f564a-3f79-4981-8814-188fe39cc918"
     method="post"
     accept-charset="utf-8"
     class="flex flex-col md:flex-row items-stretch {{ $class ?? '' }}"
@@ -8,7 +8,6 @@
     <input class="mb-2 md:mb-0" type="email" autocomplete="off" id="email" name="email" placeholder="Your e-mail address" aria-label="E-mail" required>
 
     <input type="submit" name="submit" id="submit" value="Subscribe" class="px-3 py-2 text-sm text-white bg-yellow-500 font-semibold border-t-3 border-b-3 border-yellow-700 border-t-transparent">
-
 </form>
 
 @error('email')
