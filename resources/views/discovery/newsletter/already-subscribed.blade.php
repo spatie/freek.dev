81ef5975ab4af2d5c6e8cc10c0177e71cdfaf610
Rias <hey@rias.be>
2022-07-18 10:00:14 +0200

    Use form submission instead

---
+++ resources/views/discovery/newsletter/already-subscribed.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout title="Already subscribed!">
+    <div class="markup">
+        <h1>Already subscribed!</h1>
+        <p>
+            Looks like you're already subscribed to my newsletter.
+        </p>
+        <p>
+            By clicking <a
+                href="https://twitter.com/intent/tweet?url=https%3A%2F%2Ffreek.dev%2Fnewsletter&text=I%27ve%20just%20signed%20up%20for%20the%20newsletter%20on%20Laravel%2C%20PHP%20and%20JavaScript%20by%20@freekmurze.%20You%20can%20sign%20up%20via%20this%20link%3A&hashtags=laravel%2C%20php%2C%20javascript">this
+                link</a> you can share your subscription with your followers on Twitter.
+        </p>
+    </div>
+</x-app-layout>
