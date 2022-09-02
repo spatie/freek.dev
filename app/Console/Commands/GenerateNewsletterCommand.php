31162ee0deffcbb33cb3ff0ed866dfe02a8856e3
Freek Van der Herten <freek@spatie.be>
2022-09-02 20:42:40 +0200

    wip

--- app/Console/Commands/GenerateNewsletterCommand.php
+++ app/Console/Commands/GenerateNewsletterCommand.php
@@ -2,11 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Services\Mailcoach;
 use App\Services\NewsletterGenerator;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
-use Spatie\Mailcoach\Domain\Audience\Support\Segments\EverySubscriberSegment;
-use Spatie\Mailcoach\Domain\Campaign\Models\Campaign;
 
 class GenerateNewsletterCommand extends Command
 {
@@ -14,33 +14,29 @@ class GenerateNewsletterCommand extends Command
 
     public function handle()
     {
-        /** @var Campaign $latestNewsletter */
-        $latestNewsletter = Campaign::latest()->first();
-        $latestEditionNumber = (int) Str::after($latestNewsletter->name, '#');
-        $newEditionNumber = $latestEditionNumber + 1;
-        $startDate = $latestNewsletter->created_at->addDay()->startOfDay();
+        $campaigns = Mailcoach::get('campaigns')->json()['data'];
+
+        $latestCampaignName = $campaigns[0]['name'];
+        $latestCampaignCreatedAt = Carbon::parse($campaigns[0]['created_at']);
+        $startDate = $latestCampaignCreatedAt->addDay()->startOfDay();
         $endDate = now();
 
+        $latestEditionNumber = (int)Str::after($latestCampaignName, '#');
+        $newEditionNumber = $latestEditionNumber + 1;
+
         $html = (new NewsletterGenerator(
-            $latestNewsletter->sent_at->addDay(),
+            $startDate,
             now(),
             $newEditionNumber
         ))->getHtml();
 
-        Campaign::create([
+        $response = Mailcoach::post('campaigns', [
             'name' => "freek.dev newsletter #{$newEditionNumber}",
-            'uuid' => Str::uuid(),
             'html' => $html,
             'from_email' => 'freek@spatie.be',
             'from_name' => 'Freek Van der Herten',
             'subject' => "freek.dev newsletter #{$newEditionNumber}",
-            'email_list_id' => 2,
-            'status' => 'draft',
-            'track_opens' => 0,
-            'track_clicks' => 0,
-            'utm_tags' => 1,
-            'sent_to_number_of_subscribers' => 0,
-            'segment_class' => EverySubscriberSegment::class,
+            'email_list_uuid' => '585c113f-8479-42cb-a0d4-5f472f82130e',
         ]);
 
         $this->info("freek.dev newsletter #{$newEditionNumber} has been created with posts from {$startDate->format('Y-m-d')} until {$endDate->format('Y-m-d')}!");
