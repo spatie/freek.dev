f44869e88b1057aaeba3873d53d2ada25e21995d
Freek Van der Herten <freek@spatie.be>
2022-09-25 15:49:05 +0200

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,7 @@ use App\Console\Commands\PublishScheduledPostsCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Spatie\Health\Commands\RunHealthChecksCommand;
+use Spatie\ScheduleMonitor\Models\MonitoredScheduledTaskLogItem;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,11 +14,11 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command(RunHealthChecksCommand::class)->everyMinute();
         $schedule->command(PublishScheduledPostsCommand::class)->everyMinute();
-        $schedule->command('schedule-monitor:clean')->daily();
         $schedule->command('responsecache:clear')->daily();
         $schedule->command('backup:clean')->daily()->at('01:00');
         $schedule->command('backup:run')->dailyAt('3:00');
         $schedule->command('site-search:crawl')->daily();
+        $schedule->command('model:prune', ['--model' =>  MonitoredScheduledTaskLogItem::class])->daily();
     }
 
     protected function commands()
