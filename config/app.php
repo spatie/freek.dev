d247b65cf5e310d586a3bffb28319ea57007f01b
Freek Van der Herten <freek@spatie.be>
2022-10-04 20:40:00 +0200

    wip

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
-        App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\NavigationServiceProvider::class,
         App\Services\Twitter\TwitterServiceProvider::class,
