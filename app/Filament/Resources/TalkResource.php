611e105f83768b77df0c4db17fda02ca787c021a
freekmurze <freekmurze@users.noreply.github.com>
2022-10-04 16:33:38 +0000

    Fix styling

--- app/Filament/Resources/TalkResource.php
+++ app/Filament/Resources/TalkResource.php
@@ -3,15 +3,12 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\TalkResource\Pages;
-use App\Filament\Resources\TalkResource\RelationManagers;
 use App\Models\Talk;
 use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class TalkResource extends Resource
 {
