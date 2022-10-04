55205687e088ac2082df312c85f77bfbd9799042
Freek Van der Herten <freek@spatie.be>
2022-10-04 20:52:03 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use Spatie\RouteDiscovery\Discovery\Discover;
 
 Discover::views()->in(resource_path('views/discovery'));
 Discover::controllers()->in(app_path('Http/Controllers/Discovery'));
+
+Route::redirect('/admin', '/admin/posts');
