d5e7de6c5d22d3bb76c41f2575dde5cb2c5ce25b
Freek Van der Herten <freek@spatie.be>
2022-11-11 20:48:26 +0100

    wip

--- config/horizon.php
+++ config/horizon.php
@@ -76,9 +76,9 @@ return [
                 'connection' => 'redis',
                 'queue' => ['default'],
                 'balance' => 'simple',
-                'processes' => 10,
+                'processes' => 1,
                 'tries' => 2,
-                'timeout' => 60 * 60,
+                'timeout' => 60,
             ],
         ],
 
