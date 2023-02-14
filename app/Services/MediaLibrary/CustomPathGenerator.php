2ff5e6206035999d6bd1fe29deae92f5ffb15280
freekmurze <freekmurze@users.noreply.github.com>
2023-02-14 21:59:21 +0000

    Fix styling

--- app/Services/MediaLibrary/CustomPathGenerator.php
+++ app/Services/MediaLibrary/CustomPathGenerator.php
@@ -9,9 +9,6 @@ class CustomPathGenerator implements BasePathGenerator
 {
     /**
      * Get the path for the given media, relative to the root storage path.
-     *
-     * @param  \Spatie\MediaLibrary\MediaCollections\Models\Media  $media
-     * @return string
      */
     public function getPath(Media $media): string
     {
@@ -20,9 +17,6 @@ class CustomPathGenerator implements BasePathGenerator
 
     /**
      * Get the path for conversions of the given media, relative to the root storage path.
-     *
-     * @param  \Spatie\MediaLibrary\MediaCollections\Models\Media  $media
-     * @return string
      */
     public function getPathForConversions(Media $media): string
     {
@@ -31,9 +25,6 @@ class CustomPathGenerator implements BasePathGenerator
 
     /**
      * Get the path for responsive images of the given media, relative to the root storage path.
-     *
-     * @param  \Spatie\MediaLibrary\MediaCollections\Models\Media  $media
-     * @return string
      */
     public function getPathForResponsiveImages(Media $media): string
     {
