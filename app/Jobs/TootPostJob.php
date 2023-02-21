37a52796b280212b5ccc0f158efaedf02f8f13e0
freekmurze <freekmurze@users.noreply.github.com>
2023-02-21 16:08:23 +0000

    Fix styling

--- app/Jobs/TootPostJob.php
+++ app/Jobs/TootPostJob.php
@@ -3,7 +3,6 @@
 namespace App\Jobs;
 
 use App\Models\Post;
-use App\Services\Twitter\Twitter;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
