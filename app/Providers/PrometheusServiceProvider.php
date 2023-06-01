9b0e3d09121bfd53a32288a383dbcda073175a91
Freek Van der Herten <freek@spatie.be>
2023-06-01 10:04:01 +0200

    Add HorizonStatusCollector, JobsPerMinuteCollector and RecentJobsCollector to PrometheusServiceProvider

--- app/Providers/PrometheusServiceProvider.php
+++ app/Providers/PrometheusServiceProvider.php
@@ -7,6 +7,9 @@ use Spatie\Prometheus\Collectors\Horizon\CurrentMasterSupervisorCollector;
 use Spatie\Prometheus\Collectors\Horizon\CurrentProcessesPerQueueCollector;
 use Spatie\Prometheus\Collectors\Horizon\CurrentWorkloadCollector;
 use Spatie\Prometheus\Collectors\Horizon\FailedJobsPerHourCollector;
+use Spatie\Prometheus\Collectors\Horizon\HorizonStatusCollector;
+use Spatie\Prometheus\Collectors\Horizon\JobsPerMinuteCollector;
+use Spatie\Prometheus\Collectors\Horizon\RecentJobsCollector;
 use Spatie\Prometheus\Facades\Prometheus;
 
 class PrometheusServiceProvider extends ServiceProvider
@@ -19,13 +22,9 @@ class PrometheusServiceProvider extends ServiceProvider
          */
         Prometheus::addGauge('my_gauge', function () {
             return 123.45;
-        });
+        })->helpText('this is my gauge');
 
-        /*
-         * Uncomment this line if you want to export
-         * all Horizon metrics to prometheus
-         */
-        // $this ->registerHorizonCollectors();
+        $this ->registerHorizonCollectors();
     }
 
     public function registerHorizonCollectors(): self
@@ -35,6 +34,9 @@ class PrometheusServiceProvider extends ServiceProvider
             CurrentProcessesPerQueueCollector::class,
             CurrentWorkloadCollector::class,
             FailedJobsPerHourCollector::class,
+            HorizonStatusCollector::class,
+            JobsPerMinuteCollector::class,
+            RecentJobsCollector::class,
         ]);
 
         return $this;
