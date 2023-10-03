ce38f1d5877e8d9487a0deb197673df3653da43e
Freek Van der Herten <freek@spatie.be>
2023-10-04 01:21:25 +0200

    fix post

--- resources/views/pages/uses.blade.php
+++ resources/views/pages/uses.blade.php
@@ -7,7 +7,7 @@ use function Laravel\Folio\render;
 render(function(View $view) {
     $post = Post::find(2357);
 
-    $view->with('post');
+    $view->with(compact('post'));
 }); ?>
 
 @include('front.posts.show', ['post' => $post])
