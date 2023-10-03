a41574c8c172a5788a91aa9087209f949192e828
freekmurze <freekmurze@users.noreply.github.com>
2023-10-03 21:01:19 +0000

    Fix styling

--- tests/Feature/Models/PostTest.php
+++ tests/Feature/Models/PostTest.php
@@ -14,7 +14,7 @@ beforeEach(function () {
 it('can find a post by its id slug', function () {
     $this->withoutExceptionHandling();
 
-    get('/' . $this->post->idSlug())
+    get('/'.$this->post->idSlug())
         ->assertSuccessful()
         ->assertSee($this->post->title);
 });
