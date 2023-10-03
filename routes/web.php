c1240ab95990035ef2fa3a546cad8f539bad91ff
Freek Van der Herten <freek@spatie.be>
2023-10-03 20:56:57 +0200

    Remove unnecessary code in route files

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Spatie\RouteDiscovery\Discovery\Discover;
-
-Route::redirect('/admin', '/admin/posts');
-
-//Discover::views()->in(resource_path('views/discovery'));
-//Discover::controllers()->in(app_path('Http/Controllers/Discovery'));
