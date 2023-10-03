23aac7922f56c58d9bbdb5b7a9cea592537ca324
Freek Van der Herten <freek@spatie.be>
2023-10-03 22:20:01 +0200

    cleanup

--- resources/views/vendor/mailcoach/confirmSubscription/couldNotFindSubscription.blade.php
+++
@@ -1,8 +0,0 @@
-<x-app-layout title="Could not find subscription">
-    <div class="markup">
-        <h1>Subscription failed...</h1>
-        <p>
-            We could not find your subscription to the email list. The link you clicked seems invalid.
-        </p>
-    </div>
-</x-app-layout>
