a86a1ed5cc75c023791c9fcd7656d88284e2dad8
freekmurze <freekmurze@users.noreply.github.com>
2025-03-27 14:38:44 +0000

    Fix styling

--- database/migrations/2021_04_13_142256_add_generated_conversions_field_to_media_table.php
+++ database/migrations/2021_04_13_142256_add_generated_conversions_field_to_media_table.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 return new class extends Migration
 {
