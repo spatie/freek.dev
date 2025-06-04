640ee3ed969ed8df50b6f585452bf173e7155dcf
Freek Van der Herten <freek@spatie.be>
2025-06-04 09:59:58 +0200

    wip

--- app/Filament/Resources/PostResource/Pages/CreatePost.php
+++ app/Filament/Resources/PostResource/Pages/CreatePost.php
@@ -41,5 +41,10 @@ class CreatePost extends CreateRecord
         return $data;
     }
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
 
 }
