ada76ae400d1821790fa99741b3e3de3ce799e1f
Freek Van der Herten <freek@spatie.be>
2025-08-19 02:19:11 +0200

    wip

--- routes/console.php
+++ routes/console.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\Schedule;
 use Spatie\Health\Commands\RunHealthChecksCommand;
 use Spatie\ScheduleMonitor\Models\MonitoredScheduledTaskLogItem;
 
-Schedule::command(RunHealthChecksCommand::class)->everyMinute();
-Schedule::command(PublishScheduledPostsCommand::class)->everyMinute();
+Schedule::command(RunHealthChecksCommand::class)->everyMinute()->graceTimeInMinutes(3);
+Schedule::command(PublishScheduledPostsCommand::class)->everyMinute()->graceTimeInMinutes(3);
 Schedule::command('responsecache:clear')->daily();
 Schedule::command('backup:clean')->daily()->at('01:00');
 Schedule::command('backup:run')->dailyAt('3:00');
-Schedule::command('site-search:crawl')->daily();
-Schedule::command('model:prune', ['--model' => MonitoredScheduledTaskLogItem::class])->daily();
+Schedule::command('site-search:crawl')->daily()->graceTimeInMinutes(10);
+Schedule::command('model:prune', ['--model' => MonitoredScheduledTaskLogItem::class])->daily()->graceTimeInMinutes(10);
