99b812d46ab5dec4e6d34f8a0dcd7b6723c6f078
Freek Van der Herten <freek@spatie.be>
2026-02-09 14:59:29 +0100

    wip

--- resources/views/front/speaking/partials/video.blade.php
+++ resources/views/front/speaking/partials/video.blade.php
@@ -1,5 +1,5 @@
 <article class="p-4 -mx-4 sm:mx-0 sm:p-6 m bg-gray-100 border-b-5 border-gray-200 mb-6">
-    <h3 class="h3 | mt-0 mb-4">{{ $video->title }}</h3>
+    <h3 class="h3 | !mt-0 mb-4">{{ $video->title }}</h3>
     <div class="md:flex text-sm">
         <div class="markup | mb-5 md:mb-0 md:w-1/3 md:pr-4">
             <x-lazy>
