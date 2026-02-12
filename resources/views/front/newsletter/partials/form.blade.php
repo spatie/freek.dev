71b5dd03584052007e4122793ad89427047ac74a
Freek Van der Herten <freek@spatie.be>
2026-02-12 15:33:10 +0100

    wip

--- resources/views/front/newsletter/partials/form.blade.php
+++ resources/views/front/newsletter/partials/form.blade.php
@@ -6,9 +6,9 @@
 >
     <input type="text" name="username" style="display:none !important" tabindex="-1" autocomplete="off">
 
-    <input class="mb-2 md:mb-0" type="email" autocomplete="off" id="email" name="email" placeholder="Your e-mail address" aria-label="E-mail" required>
+    <input class="mb-2 md:mb-0" type="email" autocomplete="off" id="email" name="email" placeholder="developer@awesome.dev" aria-label="E-mail" required>
 
-    <input type="submit" name="submit" id="submit" value="Subscribe" class="px-3 py-2 text-sm text-white bg-yellow-500 font-semibold border-t-3 border-b-3 border-yellow-700 border-t-transparent">
+    <input type="submit" name="submit" id="submit" value="Count me in" class="px-3 py-2 text-sm text-white bg-yellow-500 font-semibold border-t-3 border-b-3 border-yellow-700 border-t-transparent">
 </form>
 
 <x-form-error name="email" />
