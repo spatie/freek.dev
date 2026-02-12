5b2abfb4c87638f225e589781fd3dfd5c45b7153
Freek Van der Herten <freek@spatie.be>
2026-02-12 23:14:25 +0100

    wip

--- resources/views/front/components/ad.blade.php
+++ resources/views/front/components/ad.blade.php
@@ -1,5 +1,5 @@
 @if($ad)
-<div data-no-index class="markup | text-xs text-gray-700 mb-6 bg-yellow-50 p-2">
+<div data-no-index class="-mx-4 sm:mx-0 p-4 sm:p-6 md:p-8 bg-yellow-50 border-b-5 border-yellow-200 text-sm text-gray-700 mb-8 markup">
     {!! $ad->html !!}
 </div>
 @endif
