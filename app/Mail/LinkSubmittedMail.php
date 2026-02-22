5be4bd0bfacdff8bafae332b06b919519a2cf063
Freek Van der Herten <freek@spatie.be>
2026-02-22 23:24:15 +0100

    wip

--- app/Mail/LinkSubmittedMail.php
+++ app/Mail/LinkSubmittedMail.php
@@ -3,6 +3,7 @@
 namespace App\Mail;
 
 use App\Models\Link;
+use App\Models\Post;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
@@ -11,9 +12,13 @@ class LinkSubmittedMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public ?Post $existingPost = null;
+
     public function __construct(
         public Link $link
-    ) {}
+    ) {
+        $this->existingPost = Post::where('external_url', $this->link->url)->first();
+    }
 
     public function build()
     {
--- resources/views/mails/links/submitted.blade.php
+++ resources/views/mails/links/submitted.blade.php
@@ -5,6 +5,12 @@ A link titled "[{{ $link->title }}]({{ $link->url }})" was submitted by {{ $link
 
 {{ $link->text }}
 
+@if($existingPost)
+@component('mail::panel')
+⚠️ This link is already {{ $existingPost->published ? 'published' : 'scheduled' }} as [{{ $existingPost->title }}]({{ url($existingPost->url) }}) on {{ $existingPost->publish_date->format('M j, Y') }}. You probably want to reject this submission.
+@endcomponent
+@endif
+
 [Approve]({{ $link->approveUrl() }})
 
 [Approve and create post]({{ $link->approveAndCreatePostUrl() }})
