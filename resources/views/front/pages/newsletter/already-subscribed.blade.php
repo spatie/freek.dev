607c655051457a250db2b359a1277c02dce5bf4c
Freek Van der Herten <freek@spatie.be>
2026-03-04 01:36:18 +0100

    wip

--- resources/views/front/pages/newsletter/already-subscribed.blade.php
+++ resources/views/front/pages/newsletter/already-subscribed.blade.php
@@ -6,7 +6,7 @@
         </p>
         <p>
             By clicking <a
-                href="https://twitter.com/intent/tweet?url=https%3A%2F%2Ffreek.dev%2Fnewsletter&text=I%27ve%20just%20signed%20up%20for%20the%20newsletter%20on%20Laravel%2C%20PHP%20and%20JavaScript%20by%20@freekmurze.%20You%20can%20sign%20up%20via%20this%20link%3A&hashtags=laravel%2C%20php%2C%20javascript">this
+                href="https://twitter.com/intent/tweet?url=https%3A%2F%2Ffreek.dev%2Fnewsletter&text=I%27ve%20just%20signed%20up%20for%20the%20newsletter%20on%20Laravel%2C%20PHP%20and%20JavaScript%20by%20@freekmurze.%20You%20can%20sign%20up%20via%20this%20link%3A&hashtags=laravel%2Cphp%2Cjavascript">this
                 link</a> you can share your subscription with your followers on Twitter.
         </p>
     </div>
